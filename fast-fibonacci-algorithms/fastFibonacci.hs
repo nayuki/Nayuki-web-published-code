46961c3c2c1d394309792ddb4a2cab08084cf8a9
Nayuki Minase <nayuki@eigenstate.org>
2011-06-24 00:59:08 -0400

    Updated page "Fast Fibonacci algorithms", updated page "Karatsuba multiplication", added .hs type to .htaccess.

---
+++ fast-fibonacci-algorithms/fastFibonacci.hs
@@ -0,0 +1,14 @@
+-- fibonacci n = F(n)
+fibonacci :: Integer -> Integer
+fibonacci n | n >= 0 = a where (a, _) = fib n
+
+-- Internal function: fib n = (F(n), F(n+1))
+fib :: Integer -> (Integer, Integer)
+fib 0 = (0, 1)
+fib n =
+    let (a, b) = fib (div n 2)
+        c = a * (2 * b - a)
+        d = b * b + a * a
+    in if mod n 2 == 0
+        then (c, d)
+        else (d, c + d)
