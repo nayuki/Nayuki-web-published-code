9d9fcce5014a238d1f0daa7a3a62bfdabe31de5a
Nayuki Minase <nayuki@eigenstate.org>
2011-07-11 16:56:05 -0400

    Added page "Tiny Encryption Algorithm in x86 assembly", updated style.

---
+++ tiny-encryption-algorithm-in-x86-assembly/tea.s
@@ -0,0 +1,68 @@
+/* void tea_encrypt(uint32_t *msg, uint32_t *key); */
+.globl tea_encrypt
+tea_encrypt:
+	/* Enter */
+	pushl  %ebp
+	movl   %esp, %ebp
+	subl   $12, %esp
+	
+	/* Preserve callee-save registers */
+	movl   %ebx, 0(%esp)
+	movl   %esi, 4(%esp)
+	movl   %edi, 8(%esp)
+	
+	/* Load address of message and key */
+	movl    8(%ebp), %eax  /* Message */
+	movl   12(%ebp), %edx  /* Key */
+	
+	/* Load message words */
+	movl   0(%eax), %esi
+	movl   4(%eax), %edi
+	
+	/* Initialize round constant */
+	movl   $0x9E3779B9, %ecx  /* 'sum' */
+	
+.tea_encrypt_top:
+	/* Encrypt 0th message word */
+	movl   %edi, %ebx
+	shll   $4, %ebx
+	addl   0(%edx), %ebx
+	leal   (%edi,%ecx), %eax
+	xorl   %eax, %ebx
+	movl   %edi, %eax
+	shrl   $5, %eax
+	addl   4(%edx), %eax
+	xorl   %eax, %ebx
+	addl   %ebx, %esi
+	
+	/* Encrypt 1st message word */
+	movl   %esi, %ebx
+	shll   $4, %ebx
+	addl   8(%edx), %ebx
+	leal   (%esi,%ecx), %eax
+	xorl   %eax, %ebx
+	movl   %esi, %eax
+	shrl   $5, %eax
+	addl   12(%edx), %eax
+	xorl   %eax, %ebx
+	addl   %ebx, %edi
+	
+	/* Increment */
+	addl   $0x9E3779B9, %ecx
+	cmpl   $0x6526B0D9, %ecx
+	jne    .tea_encrypt_top
+	
+	/* Store message */
+	movl   8(%ebp), %eax
+	movl   %esi, 0(%eax)
+	movl   %edi, 4(%eax)
+	
+	/* Restore registers */
+	movl   0(%esp), %ebx
+	movl   4(%esp), %esi
+	movl   8(%esp), %edi
+	
+	/* Exit */
+	addl   $12, %esp
+	popl   %ebp
+	ret
---
+++ tiny-encryption-algorithm-in-x86-assembly/teatest.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/* x86 assembly version */
+void tea_encrypt(uint32_t *msg, uint32_t *key);
+
+/* C version */
+void tea_encrypt_c(uint32_t *msg, uint32_t *key) {
+	uint32_t y = msg[0], z = msg[1];
+	uint32_t k0 = key[0], k1 = key[1], k2 = key[2], k3 = key[3];
+	int i;
+	uint32_t sum;
+	for (i = 0, sum = 0x9E3779B9; i < 32; i++, sum += 0x9E3779B9) {
+		y += ((z << 4) + k0) ^ (z + sum) ^ ((z >> 5) + k1);
+		z += ((y << 4) + k2) ^ (y + sum) ^ ((y >> 5) + k3);
+	}
+	msg[0] = y;
+	msg[1] = z;
+}
+
+
+int main(int argc, char **argv) {
+	uint32_t msg[2] = {0, 0};
+	uint32_t key[4] = {0, 0, 0, 0};
+	
+	// Sanity test
+	tea_encrypt(msg, key);
+	printf("Ciphertext: %08x %08x\n", msg[0], msg[1]);  // Should be 41ea3a0a 94baa940
+	
+	// Benchmark speed
+	const int N = 10000000;
+	int i;
+	for (i = 0; i < N; i++)
+		tea_encrypt(msg, key);
+	printf("Speed: %.1f MiB/s\n", (double)N * 8 / clock() * CLOCKS_PER_SEC / 1048576);
+	
+	return 0;
+}
+
