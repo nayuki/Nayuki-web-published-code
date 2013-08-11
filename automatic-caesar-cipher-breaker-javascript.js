ae709ff9db88f0207731b789d425611eeed35929
Nayuki Minase <nayuki@eigenstate.org>
2013-08-11 00:01:42 -0400

    Updated pages "Automatic Caesar cipher breaker (JavaScript)", "Caesar cipher (JavaScript)", "Vigenere cipher (JavaScript)".

--- automatic-caesar-cipher-breaker-javascript.js
+++ automatic-caesar-cipher-breaker-javascript.js
@@ -19,6 +19,8 @@ function doBreak() {
 	entropies.sort(function(x, y) {
 		if (x[1] < y[1]) return -1;
 		else if (x[1] > y[1]) return 1;
+		else if (x[0] < y[0]) return -1;
+		else if (x[0] > y[0]) return 1;
 		else return 0;
 	});
 	
