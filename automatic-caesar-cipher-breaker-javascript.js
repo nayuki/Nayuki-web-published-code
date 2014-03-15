195d8543d28f5c7de3dfef2a92d2cd21cc6722a5
Nayuki Minase <nayuki@eigenstate.org>
2014-03-15 00:47:55 -0400

    Updated pages "Automatic Caesar cipher breaker (JavaScript)", "Good Java idioms".

--- automatic-caesar-cipher-breaker-javascript.js
+++ automatic-caesar-cipher-breaker-javascript.js
@@ -1,7 +1,7 @@
 /* 
  * Automatic Caesar cipher breaker
  * 
- * Copyright (c) 2013 Nayuki Minase
+ * Copyright (c) 2014 Nayuki Minase
  * All rights reserved. Contact Nayuki for licensing.
  * http://nayuki.eigenstate.org/page/automatic-caesar-cipher-breaker-javascript
  */
@@ -9,17 +9,16 @@
 "use strict";
 
 
-var entropies = new Array(26);
+/* User interaction functions */
+
+// Array of pairs (int shift, float entropy), sorted in ascending order of entropy
+var entropies;
 
 function doBreak() {
-	// Get entropy for all possible keys
 	var text = document.getElementById("text").value;
-	var maxEntropy = 0;
-	for (var i = 0; i < entropies.length; i++) {
-		entropies[i] = [i, getEntropy(decrypt(text, i))];
-		maxEntropy = Math.max(entropies[i][1], maxEntropy);
-	}
+	entropies = getAllEntropies(text);
 	entropies.sort(function(x, y) {
+		// Compare by lowest entropy, break ties by lowest shift
 		if (x[1] < y[1]) return -1;
 		else if (x[1] > y[1]) return 1;
 		else if (x[0] < y[0]) return -1;
@@ -35,6 +34,7 @@ function doBreak() {
 	// Build table of best guesses
 	var guessesElem = document.getElementById("guesses");
 	removeAllChildren(guessesElem);
+	var maxEntropy = entropies[entropies.length - 1][1];
 	for (var i = 0; i < entropies.length; i++) {
 		var tr = document.createElement("tr");
 		if (i == 0)
@@ -65,6 +65,17 @@ function doBreak() {
 }
 
 
+function setShift(newShift) {
+	newShift = mod(newShift, 26);
+	document.getElementById("text").value = decrypt(document.getElementById("text").value, newShift - getShift());
+	document.getElementById("shift").value = newShift.toString();
+	
+	var guessesElem = document.getElementById("guesses");
+	for (var i = 0; i < 26; i++)
+		guessesElem.childNodes[i].className = entropies[i][0] == newShift ? "active" : "";
+}
+
+
 function getShift() {  // Error-resilient
 	var shiftText = document.getElementById("shift").value;
 	var shift = parseInt(shiftText, 10);
@@ -74,48 +85,52 @@ function getShift() {  // Error-resilient
 }
 
 
-function setShift(newShift) {
-	newShift = mod(newShift, 26);
-	var oldShift = getShift();
-	var delta = mod(newShift - oldShift, 26);
-	
-	document.getElementById("text").value = decrypt(document.getElementById("text").value, delta);
-	document.getElementById("shift").value = newShift.toString();
-	
-	var guessesElem = document.getElementById("guesses");
+/* Core functions */
+
+// Returns the entropies when the given string is decrypted with all 26 possible shifts,
+// where the result is an array of pairs (int shift, float enptroy) - e.g. [[0, 2.01], [1, 4.95], ..., [25, 3.73]].
+function getAllEntropies(str) {
+	var result = new Array(26);
 	for (var i = 0; i < 26; i++)
-		guessesElem.childNodes[i].className = entropies[i][0] == newShift ? "active" : "";
+		result[i] = [i, getEntropy(decrypt(str, i))];
+	return result;
 }
 
 
+// Unigram model frequencies for letters A, B, ..., Z
+var ENGLISH_FREQS = [
+	0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406,
+	0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074];
+
+// Returns the cross-entropy of the given string with respect to the English unigram frequencies, which is a positive floating-point number.
+function getEntropy(str) {
+	var sum = 0;
+	var ignored = 0;
+	for (var i = 0; i < str.length; i++) {
+		var c = str.charCodeAt(i);
+		if      (c >= 65 && c <=  90) sum += Math.log(ENGLISH_FREQS[c - 65]);  // Uppercase
+		else if (c >= 97 && c <= 122) sum += Math.log(ENGLISH_FREQS[c - 97]);  // Lowercase
+		else ignored++;
+	}
+	return -sum / Math.log(2) / (str.length - ignored);
+}
+
+
+// Decrypts the given string with the given key using the Caesar shift cipher.
+// The key is an integer representing the number of letters to step back by - e.g. decrypt("EB", 2) = "CZ".
 function decrypt(str, key) {
-	key = mod(-key, 26);  // Change it into an encryption key
 	var result = "";
 	for (var i = 0; i < str.length; i++) {
 		var c = str.charCodeAt(i);
-		if      (c >= 65 && c <=  90) result += String.fromCharCode((c - 65 + key) % 26 + 65);  // Uppercase
-		else if (c >= 97 && c <= 122) result += String.fromCharCode((c - 97 + key) % 26 + 97);  // Lowercase
+		if      (c >= 65 && c <=  90) result += String.fromCharCode(mod(c - 65 - key, 26) + 65);  // Uppercase
+		else if (c >= 97 && c <= 122) result += String.fromCharCode(mod(c - 97 - key, 26) + 97);  // Lowercase
 		else result += str.charAt(i);  // Copy
 	}
 	return result;
 }
 
 
-// Unigram model frequencies
-var ENGLISH_FREQS = [
-	0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406,
-	0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150, 0.01974, 0.00074];
-
-function getEntropy(str) {
-	var sum = 0;
-	for (var i = 0; i < str.length; i++) {
-		var c = str.charCodeAt(i);
-		if      (c >= 65 && c <=  90) sum += Math.log(ENGLISH_FREQS[c - 65]);  // Uppercase
-		else if (c >= 97 && c <= 122) sum += Math.log(ENGLISH_FREQS[c - 97]);  // Lowercase
-	}
-	return -sum / Math.log(2) / str.length;
-}
-
+/* Utilities */
 
 function removeAllChildren(node) {
 	while (node.childNodes.length > 0)
