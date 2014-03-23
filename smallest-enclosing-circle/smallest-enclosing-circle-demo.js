a9ce17f5b45fba90047d091988568a492179c1fb
Nayuki Minase <nayuki@eigenstate.org>
2014-03-23 12:52:19 -0400

    Updated pages "Smallest enclosing circle", "Overview of Project Nayuki software licenses".

--- smallest-enclosing-circle/smallest-enclosing-circle-demo.js
+++ smallest-enclosing-circle/smallest-enclosing-circle-demo.js
@@ -2,8 +2,21 @@
  * Smallest enclosing circle
  * 
  * Copyright (c) 2014 Nayuki Minase
- * All rights reserved. Contact Nayuki for licensing.
  * http://nayuki.eigenstate.org/page/smallest-enclosing-circle
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see COPYING.txt).
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 "use strict";
