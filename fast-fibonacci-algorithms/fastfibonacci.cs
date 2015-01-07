8d74742acbecc6b48e0b42c5028a374fdc50d2e1
Nayuki Minase <nayuki@eigenstate.org>
2015-01-07 01:12:45 -0500

    Updated page "Fast Fibonacci algorithms".

---
+++ fast-fibonacci-algorithms/fastfibonacci.cs
@@ -0,0 +1,53 @@
+/* 
+ * Fast doubling Fibonacci algorithm
+ * 
+ * Copyright (c) 2015 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * http://www.nayuki.io/page/fast-fibonacci-algorithms
+ */
+
+using System;
+using System.Numerics;
+
+
+/*
+ * Run with a single number argument. Example:
+ *   Command: fastfibonacci.exe 7
+ *   Output: "fibonacci(7) = 13"
+ */
+public class fastfibonacci {
+	
+	public static int Main(string[] args) {
+		if (args.Length == 0) {
+			Console.WriteLine("Usage: fastfibonacci.exe N");
+			return 1;
+		}
+		int n = int.Parse(args[0]);
+		if (n < 0) {
+			Console.WriteLine("Number must be non-negative");
+			return 1;
+		}
+		Console.WriteLine("fibonacci({0}) = {1}", n, Fibonacci(n));
+		return 0;
+	}
+	
+	
+	// Fast doubling algorithm
+	private static BigInteger Fibonacci(int n) {
+		BigInteger a = BigInteger.Zero;
+		BigInteger b = BigInteger.One;
+		for (int i = 31; i >= 0; i--) {
+			BigInteger d = a * (b * 2 - a);
+			BigInteger e = a * a + b * b;
+			a = d;
+			b = e;
+			if ((((uint)n >> i) & 1) != 0) {
+				BigInteger c = a + b;
+				a = b;
+				b = c;
+			}
+		}
+		return a;
+	}
+	
+}
