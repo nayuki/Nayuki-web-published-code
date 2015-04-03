4bfccdc9943ed3f1676a2428026af7e1f29434b1
Nayuki Minase <nayuki@eigenstate.org>
2015-04-03 15:33:34 -0400

    Updated page "Fast Fibonacci algorithms".

--- fast-fibonacci-algorithms/fastfibonacci.hs
+++ fast-fibonacci-algorithms/fastfibonacci.hs
@@ -9,15 +9,15 @@
 
 -- (Public) fibonacci n = F(n)
 fibonacci :: Integer -> Integer
-fibonacci n | n >= 0 = a where (a, _) = fib n
+fibonacci n | n >= 0 = fst (fib n)
 
 -- (Private) fib n = (F(n), F(n+1))
 fib :: Integer -> (Integer, Integer)
 fib 0 = (0, 1)
 fib n =
 	let (a, b) = fib (div n 2)
-		c = a * (b * 2 - a)
-		d = a * a + b * b
+	    c = a * (b * 2 - a)
+	    d = a * a + b * b
 	in if mod n 2 == 0
 		then (c, d)
 		else (d, c + d)
