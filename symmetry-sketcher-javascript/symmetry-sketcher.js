4032fa28cf8da706039a4aef3766cfbe8069b09b
Nayuki Minase <nayuki@eigenstate.org>
2015-12-01 16:48:32 -0500

    Updated page "Symmetry sketcher (JavaScript)".

--- symmetry-sketcher-javascript/symmetry-sketcher.js
+++ symmetry-sketcher-javascript/symmetry-sketcher.js
@@ -12,19 +12,20 @@
 /*---- Global variables ----*/
 
 // HTML elements
-var mainCanvasElem      = element("main-canvas"     );
-var guideCanvasElem     = element("guide-canvas"    );
-var hoverCanvasElem     = element("hover-canvas"    );
-var canvasContainerElem = element("canvas-container");
-var undoButtonElem      = element("undo-button"     );
-var doneButtonElem      = element("done-button"     );
-undoButtonElem.disabled = true;  // Invariant: This is true iff undoImages.length == 0
-doneButtonElem.value = "Done";
+var canvasElem     = element("canvas");
+var undoButtonElem = element("undo-button");
+var doneButtonElem = element("done-button");
 
 // Graphics initialization
-var mainGfx  = mainCanvasElem .getContext("2d");
-var guideGfx = guideCanvasElem.getContext("2d");
-var hoverGfx = hoverCanvasElem.getContext("2d");
+var width  = canvasElem.width;
+var height = canvasElem.height;
+var baseCanvas  = document.createElement('canvas');  // Off-screen
+var guideCanvas = document.createElement('canvas');  // Off-screen
+guideCanvas.width  = baseCanvas.width  = width;
+guideCanvas.height = baseCanvas.height = height;
+var baseGfx   = baseCanvas .getContext("2d");
+var guideGfx  = guideCanvas.getContext("2d");
+var screenGfx = canvasElem .getContext("2d");
 
 // Cached values from form inputs
 var strokeWidth      = null;  // Type number, positive
@@ -35,9 +36,11 @@ var mirrorSymmetry   = null;  // Type boolean
 var showGuidelines   = null;  // Type boolean
 
 // State variables
+var isDone = true;
 var isMouseDown = false;
 var lastCoord = null;  // Is null iff isMouseDown is false
 var undoImages = [];
+undoButtonElem.disabled = undoImages.length == 0;
 
 // Internal configuration
 var MAX_UNDO_IMAGES = 5;
@@ -47,27 +50,30 @@ guideGfx.strokeStyle = "#C0C0FF";
 
 /*---- Drawing functions ----*/
 
+// Refreshes the on-screen canvas, and draws onto it a cursor centered at the given coordinates.
 function drawHover(x, y) {
-	hoverGfx.clearRect(0, 0, hoverCanvasElem.width, hoverCanvasElem.height);
-	hoverGfx.fillStyle = backgroundColor;
+	screenGfx.drawImage(guideCanvas, 0, 0);
+	screenGfx.fillStyle = backgroundColor;
 	var temp = strokeWidth;
 	strokeWidth *= 1.2;
-	drawPoint(hoverGfx, x, y);
+	drawPoint(screenGfx, x, y);
 	strokeWidth = temp;
-	hoverGfx.fillStyle = paintColor;
-	drawPoint(hoverGfx, x, y);
+	screenGfx.fillStyle = paintColor;
+	drawPoint(screenGfx, x, y);
 	
 	var crossSize = Math.max(strokeWidth * 2.5, 10);
-	hoverGfx.lineWidth = Math.max(strokeWidth / 8, 1);
-	hoverGfx.beginPath();
-	hoverGfx.moveTo(x - crossSize / 2, y);
-	hoverGfx.lineTo(x + crossSize / 2, y);
-	hoverGfx.moveTo(x, y - crossSize / 2);
-	hoverGfx.lineTo(x, y + crossSize / 2);
-	hoverGfx.stroke();
+	screenGfx.lineWidth = Math.max(strokeWidth / 8, 1);
+	screenGfx.beginPath();
+	screenGfx.moveTo(x - crossSize / 2, y);
+	screenGfx.lineTo(x + crossSize / 2, y);
+	screenGfx.moveTo(x, y - crossSize / 2);
+	screenGfx.lineTo(x, y + crossSize / 2);
+	screenGfx.stroke();
 }
 
 
+// Symmetrizes the point at the given coordinates, and draws them on the given
+// graphics context,using the global stroke width and the graphics context's stroke style.
 function drawPoint(gfx, x, y) {
 	getSymmetryPoints(x, y).forEach(function(coord) {
 		gfx.beginPath();
@@ -77,6 +83,8 @@ function drawPoint(gfx, x, y) {
 }
 
 
+// Symmetrizes the line between the given coordinates, and draws them on the given
+// graphics context, using the global stroke width and the graphics context's stroke style.
 function drawLine(gfx, x0, y0, x1, y1) {
 	var starts = getSymmetryPoints(x0, y0);
 	var ends   = getSymmetryPoints(x1, y1);
@@ -90,11 +98,13 @@ function drawLine(gfx, x0, y0, x1, y1) {
 }
 
 
+// Returns an array of pairs representing the symmetries of the given point coordinates,
+// based on the current global rotation symmetry and mirror settings.
 function getSymmetryPoints(x, y) {
 	// The coordinate system has its origin at the center of the canvas,
 	// has up as 0 degrees, right as 90 deg, down as 180 deg, and left as 270 deg.
-	var ctrX = mainCanvasElem.width  / 2;
-	var ctrY = mainCanvasElem.height / 2;
+	var ctrX = width  / 2;
+	var ctrY = height / 2;
 	var relX = x - ctrX;
 	var relY = ctrY - y;
 	var dist  = Math.hypot(relX, relY);
@@ -114,14 +124,14 @@ function getSymmetryPoints(x, y) {
 }
 
 
-function drawGuidelines() {
-	guideGfx.clearRect(0, 0, guideCanvasElem.width, guideCanvasElem.height);
+// Refreshes the guide canvas based on the base canvas, and draws guidelines if enabled.
+function redrawGuideCanvas() {
+	guideGfx.drawImage(baseCanvas, 0, 0);
 	if (!showGuidelines)
 		return;
 	
-	var halfwidth  = guideCanvasElem.width  / 2;
-	var halfheight = guideCanvasElem.height / 2;
-	guideGfx.clearRect(0, 0, halfwidth * 2, halfheight * 2);
+	var halfwidth  = width  / 2;
+	var halfheight = height / 2;
 	guideGfx.lineWidth = 1;
 	guideGfx.beginPath();
 	var dist = Math.min(halfwidth, halfheight);
@@ -145,17 +155,20 @@ function drawGuidelines() {
 
 
 function updatePaintColor() {
-	mainGfx.strokeStyle  = paintColor;
-	mainGfx.fillStyle    = paintColor;
-	hoverGfx.strokeStyle = paintColor;
-	hoverGfx.fillStyle   = paintColor;
+	baseGfx.strokeStyle   = paintColor;
+	baseGfx.fillStyle     = paintColor;
+	screenGfx.strokeStyle = paintColor;
+	screenGfx.fillStyle   = paintColor;
 }
 
 
-function clearMainCanvas() {
-	mainGfx.fillStyle = backgroundColor;
-	mainGfx.fillRect(0, 0, mainCanvasElem.width, mainCanvasElem.height);
-	mainGfx.fillStyle = paintColor;
+// Clears the base canvas, redraws the guide canvas, and redraws the on-screen canvas.
+function clearBaseCanvas() {
+	baseGfx.fillStyle = backgroundColor;
+	baseGfx.fillRect(0, 0, width, height);
+	baseGfx.fillStyle = paintColor;
+	redrawGuideCanvas();
+	screenGfx.drawImage(guideCanvas, 0, 0);
 }
 
 
@@ -169,49 +182,60 @@ function getLocalCoordinates(ev) {
 
 // Handlers for canvas element
 
-hoverCanvasElem.onmouseover = function(ev) {
+canvasElem.onmouseover = function(ev) {
+	if (isDone)
+		return;
 	var coord = getLocalCoordinates(ev);
 	drawHover(coord[0], coord[1]);
 };
 
-hoverCanvasElem.onmousemove = function(ev) {
+canvasElem.onmousemove = function(ev) {
+	if (isDone)
+		return;
 	var coord = getLocalCoordinates(ev);
-	drawHover(coord[0], coord[1]);
 	if (isMouseDown) {
-		drawPoint(mainGfx, coord[0], coord[1]);
+		drawPoint(baseGfx, coord[0], coord[1]);
 		if (lastCoord != null)
-			drawLine(mainGfx, coord[0], coord[1], lastCoord[0], lastCoord[1]);
+			drawLine(baseGfx, coord[0], coord[1], lastCoord[0], lastCoord[1]);
+		redrawGuideCanvas();
 		lastCoord = coord;
 	}
+	drawHover(coord[0], coord[1]);
 };
 
-hoverCanvasElem.onmouseout = function() {
-	hoverGfx.clearRect(0, 0, hoverCanvasElem.width, hoverCanvasElem.height);
+canvasElem.onmouseout = function() {
+	if (isDone)
+		return;
+	screenGfx.drawImage(guideCanvas, 0, 0);
 	this.onmouseup();
 };
 
-hoverCanvasElem.onmousedown = function(ev) {
-	if (!isMouseDown) {
-		undoImages.push(mainGfx.getImageData(0, 0, mainCanvasElem.width, mainCanvasElem.height));
+canvasElem.onmousedown = function(ev) {
+	if (!isDone && !isMouseDown) {
+		undoImages.push(baseGfx.getImageData(0, 0, width, height));
 		if (undoImages.length > MAX_UNDO_IMAGES)
 			undoImages.splice(0, undoImages.length - MAX_UNDO_IMAGES);
 		undoButtonElem.disabled = undoImages.length == 0;
 		isMouseDown = true;
 		var coord = getLocalCoordinates(ev);
-		drawPoint(mainGfx, coord[0], coord[1]);
+		drawPoint(baseGfx, coord[0], coord[1]);
+		redrawGuideCanvas();
+		drawHover(coord[0], coord[1]);
 		lastCoord = coord;
 	}
 };
 
-hoverCanvasElem.onmouseup = function() {
+canvasElem.onmouseup = function() {
 	isMouseDown = false;
 	lastCoord = null;
 };
 
-hoverCanvasElem.onwheel = function(ev) {
+canvasElem.onwheel = function(ev) {
+	if (isDone)
+		return;
 	if (!isMouseDown && !isNaN(strokeWidth)) {
 		// Scroll up to increase stroke width, down to decrease
-		var step = -ev.deltaY / Math.abs(ev.deltaY);
+		var step = -ev.deltaY / Math.abs(ev.deltaY);  // Signum
 		strokeWidth *= Math.pow(10, step / 10);
 		strokeWidth = Math.max(strokeWidth, 0.1);
 		strokeWidth = Math.min(strokeWidth, 300);
@@ -222,12 +246,13 @@ hoverCanvasElem.onwheel = function(ev) {
 	return false;
 };
 
-hoverCanvasElem.oncontextmenu = function() {
-	return false;
+canvasElem.oncontextmenu = function() {
+	if (!isDone)
+		return false;
 };
 
 
-// Handlers for input form elements
+// Handlers for form input elements
 
 setAndCallHandler("stroke-width", "oninput", function() {
 	strokeWidth = parseFloat(this.value);
@@ -244,44 +269,54 @@ setAndCallHandler("background-color", "oninput", function() {
 
 setAndCallHandler("rotation-symmetry", "oninput", function() {
 	rotationSymmetry = parseInt(this.value, 10);
-	drawGuidelines();
+	redrawGuideCanvas();
+	screenGfx.drawImage(guideCanvas, 0, 0);
 });
 
 setAndCallHandler("mirror-symmetry", "onchange", function() {
 	mirrorSymmetry = this.checked;
-	drawGuidelines();
+	redrawGuideCanvas();
+	screenGfx.drawImage(guideCanvas, 0, 0);
 });
 
 setAndCallHandler("show-guidelines", "onchange", function() {
 	showGuidelines = this.checked;
-	drawGuidelines();
+	redrawGuideCanvas();
+	screenGfx.drawImage(guideCanvas, 0, 0);
 });
 
 
+// Handlers for button elements
+
 element("clear-button").onclick = function() {
 	if (confirm("Clear the drawing?")) {
-		clearMainCanvas();
-		canvasContainerElem.className = "";
-		doneButtonElem.value = "Done";
+		clearBaseCanvas();
+		if (isDone)
+			doneButtonElem.onclick();
 		undoImages = [];
 		undoButtonElem.disabled = true;
 	}
 };
 
 undoButtonElem.onclick = function() {
-	mainGfx.putImageData(undoImages.pop(), 0, 0);
+	baseGfx.putImageData(undoImages.pop(), 0, 0);
 	this.disabled = undoImages.length == 0;
+	redrawGuideCanvas();
+	screenGfx.drawImage(guideCanvas, 0, 0);
 };
 
 doneButtonElem.onclick = function() {
-	if (this.value == "Done") {
-		canvasContainerElem.className = "done";
+	if (isDone) {
+		this.value = "Done";
+		isDone = false;
+		canvasElem.style.removeProperty("cursor");
+	} else {
 		this.value = "Resume";
+		isDone = true;
 		undoImages = [];
 		undoButtonElem.disabled = true;
-	} else {
-		canvasContainerElem.className = "";
-		this.value = "Done";
+		screenGfx.drawImage(baseCanvas, 0, 0);
+		canvasElem.style.cursor = "unset";
 	}
 };
 
@@ -292,6 +327,7 @@ document.documentElement.onkeypress = function(ev) {
 	}
 };
 
+// For the text boxes, confine keystrokes to the element itself and not propagate to the document root
 (function() {
 	var elems = document.getElementsByTagName("input");
 	for (var i = 0; i < elems.length; i++)
@@ -317,4 +353,5 @@ function element(name) {
 }
 
 
-clearMainCanvas();
+clearBaseCanvas();
+doneButtonElem.onclick();
