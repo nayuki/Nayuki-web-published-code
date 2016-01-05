51f9246abcf498bc657146611c3fbaffa1378d49
Nayuki Minase <nayuki@eigenstate.org>
2016-01-05 00:54:14 -0500

    Updated pages "Forcing a file's CRC to any value", "Overview of Project Nayuki software licenses".

--- forcing-a-files-crc-to-any-value/forcecrc32.c
+++ forcing-a-files-crc-to-any-value/forcecrc32.c
@@ -2,8 +2,21 @@
  * CRC-32 forcer (C)
  * 
  * Copyright (c) 2016 Project Nayuki
- * All rights reserved. Contact Nayuki for licensing.
  * http://www.nayuki.io/page/forcing-a-files-crc-to-any-value
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see COPYING.txt).
+ * If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <inttypes.h>
--- forcing-a-files-crc-to-any-value/forcecrc32.py
+++ forcing-a-files-crc-to-any-value/forcecrc32.py
@@ -3,8 +3,21 @@
 # Compatible with Python 2 and 3.
 # 
 # Copyright (c) 2015 Project Nayuki
-# All rights reserved. Contact Nayuki for licensing.
 # http://www.nayuki.io/page/forcing-a-files-crc-to-any-value
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program (see COPYING.txt).
+# If not, see <http://www.gnu.org/licenses/>.
 # 
 
 import os, sys, zlib
