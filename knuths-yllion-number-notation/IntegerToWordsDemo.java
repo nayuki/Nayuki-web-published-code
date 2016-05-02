09f85c5e20e991731221e649c518035d9ad5094c
Nayuki Minase <nayuki@eigenstate.org>
2016-05-02 15:52:22 -0400

    Added page "Knuth's -yllion number notation".

---
+++ knuths-yllion-number-notation/IntegerToWordsDemo.java
@@ -0,0 +1,225 @@
+/* 
+ * Knuth's -yllion number notation demo
+ * 
+ * Run main program with no arguments. Prints stuff to standard output. For Java 8+.
+ * 
+ * Copyright (c) 2016 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/knuths-yllion-number-notation
+ */
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+public final class IntegerToWordsDemo {
+	
+	public static void main(String[] args) {
+		Random rand = new Random();
+		for (int i = 4; ; i++) {
+			// Choose a random positive number that is exactly 'bits' bits long
+			int bits = (int)Math.round(Math.pow(2, i / 2.0));
+			BigInteger n = new BigInteger(bits, rand);
+			n = n.setBit(bits - 1);
+			int len = n.toString().length();  // Number of digits in base 10, i.e. floor(log10(n))+1
+			if (len > 8192)
+				break;
+			System.out.printf("(%d bits, %d digits) %d%n", n.bitLength(), len, n);
+			
+			// Print number in various notations
+			if (len <= 69) {
+				System.out.println(ConventionalEnglishNotation.toStringWithCommas(n));
+				System.out.println(ConventionalEnglishNotation.numberToWords(n));
+			}
+			if (len <= 8192) {
+				System.out.println(YllionEnglishNotation.toStringWithSeparators(n));
+				System.out.println(YllionEnglishNotation.numberToWords(n));
+				System.out.println(YllionChineseNotation.numberToWords(n));
+			}
+			System.out.println();
+		}
+	}
+	
+}
+
+
+
+final class ConventionalEnglishNotation {
+	
+	// For example: numberToWords(1234567) -> "one million two hundred thirty-four thousand five hundred sixty-seven".
+	public static String numberToWords(BigInteger n) {
+		// Simple cases
+		if (n.signum() == -1)
+			return "negative " + numberToWords(n.negate());
+		else if (n.signum() == 0)
+			return "zero";
+		
+		// 1 <= n <= 999
+		else if (n.compareTo(BI_THOUSAND) < 0) {
+			int m = n.intValue();
+			String s = "";
+			if (m >= 100) {
+				s += ONES[m / 100] + " hundred";
+				if (m % 100 != 0)
+					s += " ";
+			}
+			m %= 100;
+			s += TENS[m / 10];
+			if (m < 20)
+				s += ONES[m];
+			else if (m % 10 != 0)
+				s += "-" + ONES[m % 10];
+			return s;
+		}
+		
+		else {  // n >= 1000
+			List<String> parts = new ArrayList<>();
+			for (int i = 0; n.signum() != 0; i++) {
+				if (i >= ILLIONS.length)
+					throw new IllegalArgumentException("Number too large");
+				BigInteger[] quotrem = n.divideAndRemainder(BI_THOUSAND);
+				if (quotrem[1].signum() == 1) {
+					String s = numberToWords(quotrem[1]);
+					if (i > 0)
+						s += " " + ILLIONS[i];
+					parts.add(s);
+				}
+				n = quotrem[0];
+			}
+			Collections.reverse(parts);
+			return String.join(" ", parts);
+		}
+	}
+	
+	
+	// For example: toStringWithCommas(-123456789) -> "-123,456,798".
+	public static String toStringWithCommas(BigInteger n) {
+		if (n.signum() == -1)
+			return "-" + toStringWithCommas(n.negate());
+		else {
+			StringBuilder sb = new StringBuilder(n.toString());
+			for (int i = sb.length() - 3; i > 0; i -= 3)
+				sb.insert(i, ",");
+			return sb.toString();
+		}
+	}
+	
+	
+	private static final String[] ONES = {
+		"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	
+	private static final String[] TENS = {
+		"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	
+	private static final String[] ILLIONS = {
+		null, "thousand", "million", "billion", "trillion", "quadrillion",
+		"quintillion", "sextillion", "septillion", "octillion", "nonillion",
+		"decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion",
+		"quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion",
+		"vigintillion"};
+	
+	
+	private static final BigInteger BI_THOUSAND = BigInteger.valueOf(1000);
+	
+}
+
+
+
+// Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+final class YllionEnglishNotation {
+	
+	public static String numberToWords(BigInteger n) {
+		// Simple cases
+		if (n.signum() == -1)
+			return "negative " + numberToWords(n.negate());
+		else if (n.signum() == 0)
+			return "zero";
+		
+		// 1 <= n <= 99, borrow functionality from another class
+		else if (n.compareTo(BigInteger.valueOf(100)) < 0)
+			return ConventionalEnglishNotation.numberToWords(n);
+		
+		else {  // n >= 100
+			String temp = n.toString();
+			int yllionsLen = YLLIONS.length;
+			if (temp.length() > (1 << yllionsLen))
+				throw new IllegalArgumentException("Number too large");
+			for (int i = yllionsLen - 1; i >= 1; i--) {
+				if (temp.length() > (1 << i)) {
+					int split = temp.length() - (1 << i);
+					BigInteger high = new BigInteger(temp.substring(0, split));
+					BigInteger low = new BigInteger(temp.substring(split));
+					return (high.signum() > 0 ? numberToWords(high) + " " + YLLIONS[i] : "")
+						+ (high.signum() > 0 && low.signum() > 0 ? " " : "")
+						+ (low.signum() > 0 ? numberToWords(low) : "");
+				}
+			}
+			throw new AssertionError();
+		}
+	}
+	
+	
+	// For example: toStringWithSeparators(12345678901234567890) -> "1234:5678,9012;3456,7890".
+	public static String toStringWithSeparators(BigInteger n) {
+		if (n.signum() == -1)
+			return "-" + toStringWithSeparators(n.negate());
+		else {
+			StringBuilder sb = new StringBuilder(n.toString());
+			for (int i = sb.length() - 4, j = 1; i > 0; i -= 4, j++) {
+				int k = Math.min(Integer.numberOfTrailingZeros(j), SEPARATORS.length - 1);
+				sb.insert(i, SEPARATORS[k]);
+			}
+			return sb.toString();
+		}
+	}
+	
+	
+	private static final String[] YLLIONS = {
+		null, "hundred", "myriad", "myllion", "byllion", "tryllion", "quadryllion",
+		"quintyllion", "sextyllion", "septyllion", "octyllion", "nonyllion", "decyllion"};
+	
+	private static final String[] SEPARATORS = {",", ";", ":", "'"};
+	
+}
+
+
+
+// Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+final class YllionChineseNotation {
+	
+	public static String numberToWords(BigInteger n) {
+		if (n.signum() == -1)
+			return "負" + numberToWords(n.negate());
+		else if (n.signum() == 0)
+			return "零";
+		else if (n.compareTo(BigInteger.valueOf(100)) < 0) {
+			int m = n.intValue();
+			return (m >= 10 ? (m >= 20 ? ONES[m / 10] : "") + "十" : "") + (m % 10 != 0 ? ONES[m % 10] : "");
+		} else {
+			String temp = n.toString();
+			int yllionsLen = YLLIONS.length;
+			if (temp.length() > (1 << yllionsLen))
+				throw new IllegalArgumentException("Number too large");
+			for (int i = yllionsLen - 1; i >= 1; i--) {
+				if (temp.length() > (1 << i)) {
+					int split = temp.length() - (1 << i);
+					BigInteger high = new BigInteger(temp.substring(0, split));
+					BigInteger low = new BigInteger(temp.substring(split));
+					return (high.signum() > 0 ? numberToWords(high) + YLLIONS[i] : "")
+						+ (low.signum() > 0 ? numberToWords(low) : "");
+				}
+			}
+			throw new AssertionError();
+		}
+	}
+	
+	
+	private static final String[] ONES = {null, "一", "二", "三", "四", "五", "六", "七", "八", "九"};
+	
+	private static final String[] YLLIONS = {null, "百", "萬", "億", "兆", "京", "垓", "秭", "穰", "溝", "澗", "正", "載"};
+	
+}
---
+++ knuths-yllion-number-notation/integer-to-words.py
@@ -0,0 +1,209 @@
+# 
+# Knuth's -yllion number notation demo
+# 
+# Run main program with no arguments. Prints stuff to standard output. For Python 2 and 3.
+# 
+# Copyright (c) 2016 Project Nayuki
+# All rights reserved. Contact Nayuki for licensing.
+# https://www.nayuki.io/page/knuths-yllion-number-notation
+# 
+
+import codecs, itertools, random, sys
+
+
+# ---- Main runnable demo ----
+
+def main():
+	if sys.version_info[0] >= 3:
+		# Monkey-patch stdout to accept Unicode strings
+		sys.stdout = codecs.getwriter("UTF-8")(sys.stdout.buffer)
+	
+	for i in itertools.count(4):
+		# Choose a random positive number that is exactly 'bits' bits long
+		bits = int(round(2 ** (i / 2.0)))
+		n = random.randrange(1 << (bits - 1), 1 << bits)
+		numdigits = len(str(n))  # Number of digits in base 10, i.e. floor(log10(n))+1
+		if numdigits > 8192:
+			break
+		print("({} bits, {} digits) {}".format(n.bit_length(), numdigits, n))
+		
+		# Print number in various notations
+		if numdigits <= 69:
+			print(ConventionalEnglishNotation.to_string_with_commas(n))
+			print(ConventionalEnglishNotation.number_to_words(n))
+		if numdigits <= 8192:
+			print(YllionEnglishNotation.to_string_with_separators(n))
+			print(YllionEnglishNotation.number_to_words(n))
+			chinese = YllionChineseNotation.number_to_words(n)
+			if sys.version_info[0] >= 3:
+				print(chinese)
+			else:
+				print(chinese.encode("UTF-8"))
+		print("")
+
+
+
+# ---- Submodules ----
+
+class ConventionalEnglishNotation(object):
+	
+	# For example: number_to_words(1234567) -> "one million two hundred thirty-four thousand five hundred sixty-seven".
+	@staticmethod
+	def number_to_words(n):
+		# Simple cases
+		if n < 0:
+			return "negative " + ConventionalEnglishNotation.number_to_words(-n)
+		elif n == 0:
+			return "zero"
+		
+		# 1 <= n <= 999
+		elif n < 1000:
+			s = ""
+			if n >= 100:
+				s += ConventionalEnglishNotation._ONES[n // 100] + " hundred"
+				if n % 100 != 0:
+					s += " "
+			n %= 100
+			s += ConventionalEnglishNotation._TENS[n // 10]
+			if n < 20:
+				s += ConventionalEnglishNotation._ONES[n]
+			elif n % 10 != 0:
+				s += "-" + ConventionalEnglishNotation._ONES[n % 10]
+			return s
+		
+		else:  # n >= 1000
+			parts = []
+			for i in itertools.count():
+				if n == 0:
+					break
+				if i >= len(ConventionalEnglishNotation._ILLIONS):
+					raise ValueError("Number too large")
+				rem = n % 1000
+				if rem > 0:
+					s = ConventionalEnglishNotation.number_to_words(rem)
+					if i > 0:
+						s += " " + ConventionalEnglishNotation._ILLIONS[i]
+					parts.append(s)
+				n //= 1000
+			parts.reverse()
+			return " ".join(parts)
+	
+	
+	# For example: to_string_with_commas(-123456789) -> "-123,456,798".
+	@staticmethod
+	def to_string_with_commas(n):
+		if n < 0:
+			return "-" + ConventionalEnglishNotation.to_string_with_commas(-n)
+		else:
+			s = str(n)
+			for i in range(len(s) - 3, 0, -3):
+				s = s[ : i] + "," + s[i : ]
+			return s
+	
+	
+	_ONES = [
+		"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+		"ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+	
+	_TENS = [
+		"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+	
+	_ILLIONS = [
+		None, "thousand", "million", "billion", "trillion", "quadrillion",
+		"quintillion", "sextillion", "septillion", "octillion", "nonillion",
+		"decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion",
+		"quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion",
+		"vigintillion"]
+
+
+
+# Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+class YllionEnglishNotation(object):
+	
+	@staticmethod
+	def number_to_words(n):
+		# Simple cases
+		if n < 0:
+			return "negative " + YllionEnglishNotation.number_to_words(-n)
+		elif n == 0:
+			return "zero"
+		
+		elif n < 100:  # 1 <= n <= 99, borrow functionality from another class
+			return ConventionalEnglishNotation.number_to_words(n)
+		
+		else:  # n >= 100
+			temp = str(n)
+			yllionslen = len(YllionEnglishNotation._YLLIONS)
+			if len(temp) > (1 << yllionslen):
+				raise ValueError("Number too large")
+			for i in reversed(range(1, yllionslen)):
+				if len(temp) > (1 << i):
+					high = int(temp[ : -(1 << i)])
+					low  = int(temp[-(1 << i) : ])
+					return ((YllionEnglishNotation.number_to_words(high) + " " + YllionEnglishNotation._YLLIONS[i]) if (high > 0) else "") \
+						+ (" " if (high > 0 and low > 0) else "") \
+						+ (YllionEnglishNotation.number_to_words(low) if low > 0 else "")
+			raise AssertionError()
+	
+	
+	# For example: to_string_with_separators(12345678901234567890) -> "1234:5678,9012;3456,7890".
+	@staticmethod
+	def to_string_with_separators(n):
+		if n < 0:
+			return "-" + YllionEnglishNotation.to_string_with_separators(-n)
+		else:
+			s = str(n)
+			j = 1
+			for i in range(len(s) - 4, 0, -4):
+				temp = bin(j)
+				k = len(temp) - len(temp.rstrip("0"))
+				k = min(len(YllionEnglishNotation._SEPARATORS) - 1, k)
+				s = s[ : i] + YllionEnglishNotation._SEPARATORS[k] + s[i : ]
+				j += 1
+			return s
+	
+	
+	_YLLIONS = [
+		None, "hundred", "myriad", "myllion", "byllion", "tryllion", "quadryllion",
+		"quintyllion", "sextyllion", "septyllion", "octyllion", "nonyllion", "decyllion"]
+	
+	_SEPARATORS = [",", ";", ":", "'"]
+
+
+
+# Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+class YllionChineseNotation(object):
+	
+	@staticmethod
+	def number_to_words(n):
+		if n < 0:
+			return u"\u8CA0" + YllionChineseNotation.number_to_words(-n)
+		elif n == 0:
+			return u"\u96F6"
+		elif n < 100:
+			return (((YllionChineseNotation._ONES[n // 10] if (n >= 20) else u"") + u"\u5341") if (n >= 10) else u"") \
+				+ (YllionChineseNotation._ONES[n % 10] if (n % 10 != 0) else u"")
+		else:
+			temp = str(n)
+			yllionslen = len(YllionChineseNotation._YLLIONS)
+			if len(temp) > (1 << yllionslen):
+				raise ValueError("Number too large")
+			for i in reversed(range(1, yllionslen)):
+				if len(temp) > (1 << i):
+					high = int(temp[ : -(1 << i)])
+					low  = int(temp[-(1 << i) : ])
+					return ((YllionChineseNotation.number_to_words(high) + YllionChineseNotation._YLLIONS[i]) if (high > 0) else u"") \
+						+ (YllionChineseNotation.number_to_words(low) if (low > 0) else u"")
+			raise AssertionError()
+	
+	
+	_ONES = [None, u"\u4E00", u"\u4E8C", u"\u4E09", u"\u56DB", u"\u4E94", u"\u516D", u"\u4E03", u"\u516B", u"\u4E5D"]
+	
+	_YLLIONS = [None, u"\u767E", u"\u842C", u"\u5104", u"\u5146", u"\u4EAC", u"\u5793", u"\u79ED", u"\u7A70", u"\u6E9D", u"\u6F97", u"\u6B63", u"\u8F09"]
+
+
+
+# ---- Application launcher ----
+
+if __name__ == "__main__":
+	main()
