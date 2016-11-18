22003ffb91fd6f63e206dd15b870065f664976f8
Elliott Mitchell <ehem+nayuki@m5p.com>
2016-11-18 13:44:29 -0800

    Remove extraneous whitespace
    This project seems a bit spacey to me, perhaps Mr Sed would oblige me in
    removing the end of line spaces and tabs?  Thank you Mr Sed.

--- forcing-a-files-crc-to-any-value/forcecrc32main.c
+++ forcing-a-files-crc-to-any-value/forcecrc32main.c
@@ -1,6 +1,6 @@
-/* 
+/*
  * CRC-32 forcer (C)
- * 
+ *
  * Copyright (c) 2016 Project Nayuki
  * https://www.nayuki.io/page/forcing-a-files-crc-to-any-value
  *
@@ -12,12 +12,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program (see COPYING.txt).
  * If not, see <http://www.gnu.org/licenses/>.
