55279da448974b1b3bb49c01eba29c12f18a5729
Project Nayuki <me@nayuki.io>
2017-05-21 02:43:33 -0400

    Updated pages "Transcription of Pokemon Game Boy music", "How to implement the discrete Fourier transform".

--- how-to-implement-the-discrete-fourier-transform/Dft.cs
+++ how-to-implement-the-discrete-fourier-transform/Dft.cs
@@ -5,6 +5,7 @@
  */
 
 using System;
+using System.Numerics;
 
 
 public sealed class Dft {
@@ -13,18 +14,15 @@ public sealed class Dft {
 	 * Computes the discrete Fourier transform (DFT) of the given complex vector.
 	 * All the array arguments must be non-null and have the same length.
 	 */
-	public static void computeDft(double[] inreal, double[] inimag, double[] outreal, double[] outimag) {
-		int n = inreal.Length;
+	public static void computeDft(Complex[] input, Complex[] output) {
+		int n = input.Length;
 		for (int k = 0; k < n; k++) {  // For each output element
-			double sumreal = 0;
-			double sumimag = 0;
+			Complex sum = new Complex(0, 0);
 			for (int t = 0; t < n; t++) {  // For each input element
 				double angle = 2 * Math.PI * t * k / n;
-				sumreal +=  inreal[t] * Math.Cos(angle) + inimag[t] * Math.Sin(angle);
-				sumimag += -inreal[t] * Math.Sin(angle) + inimag[t] * Math.Cos(angle);
+				sum += input[t] * Complex.Exp(new Complex(0, -angle));
 			}
-			outreal[k] = sumreal;
-			outimag[k] = sumimag;
+			output[k] = sum;
 		}
 	}
 	
