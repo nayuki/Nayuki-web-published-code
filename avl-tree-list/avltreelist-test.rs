12e81e2f2d073eddb9efb1baae2c7e106a105edb
Project Nayuki <me@nayuki.io>
2017-11-06 15:28:30 -0500

    Updated pages "AVL tree list", "Overview of Project Nayuki software licenses".

---
+++ avl-tree-list/avltreelist-test.rs
@@ -0,0 +1,416 @@
+/* 
+ * AVL tree list test (Rust)
+ * 
+ * Copyright (c) 2017 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/avl-tree-list
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+extern crate rand;
+use rand::distributions::IndependentSample;
+use rand::distributions::range::Range;
+mod avltreelist;
+use avltreelist::AvlTreeList;
+
+
+fn main() {
+	test_add();
+	test_add_list();
+	test_set();
+	test_insert_at_beginning();
+	test_insert_at_end();
+	test_insert_at_middle();
+	test_insert_list();
+	test_insert_many_beginning();
+	test_insert_many_end();
+	test_insert_many_everywhere();
+	test_remove();
+	test_clear();
+	test_against_rust_vec_randomly();
+}
+
+
+fn test_add() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.push("January");
+	list.push("February");
+	list.push("March");
+	list.push("April");
+	list.push("May");
+	list.push("June");
+	list.check_structure();
+	assert_eq!(list.len(), 6);
+	assert_eq!(*list.get(0), "January" );
+	assert_eq!(*list.get(1), "February");
+	assert_eq!(*list.get(2), "March"   );
+	assert_eq!(*list.get(3), "April"   );
+	assert_eq!(*list.get(4), "May"     );
+	assert_eq!(*list.get(5), "June"    );
+}
+
+
+fn test_add_list() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.append(&mut vec!["January"]);
+	list.append(&mut vec!["February", "March", "April"]);
+	list.append(&mut vec!["May", "June", "July", "August", "September", "October", "November", "December"]);
+	assert_eq!(list.len(), 12);
+	assert_eq!(*list.get( 0), "January"  );
+	assert_eq!(*list.get( 1), "February" );
+	assert_eq!(*list.get( 2), "March"    );
+	assert_eq!(*list.get( 3), "April"    );
+	assert_eq!(*list.get( 4), "May"      );
+	assert_eq!(*list.get( 5), "June"     );
+	assert_eq!(*list.get( 6), "July"     );
+	assert_eq!(*list.get( 7), "August"   );
+	assert_eq!(*list.get( 8), "September");
+	assert_eq!(*list.get( 9), "October"  );
+	assert_eq!(*list.get(10), "November" );
+	assert_eq!(*list.get(11), "December" );
+}
+
+
+fn test_set() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	for _ in 0 .. 10 {
+		list.push("");
+	}
+	list.set(0, "zero");
+	list.set(1, "ten");
+	list.set(2, "twenty");
+	list.set(3, "thirty");
+	list.set(4, "forty");
+	list.set(5, "fifty");
+	list.set(6, "sixty");
+	list.set(7, "seventy");
+	list.set(8, "eighty");
+	list.set(9, "ninety");
+	assert_eq!(list.len(), 10);
+	assert_eq!(*list.get(0), "zero"   );
+	assert_eq!(*list.get(1), "ten"    );
+	assert_eq!(*list.get(2), "twenty" );
+	assert_eq!(*list.get(3), "thirty" );
+	assert_eq!(*list.get(4), "forty"  );
+	assert_eq!(*list.get(5), "fifty"  );
+	assert_eq!(*list.get(6), "sixty"  );
+	assert_eq!(*list.get(7), "seventy");
+	assert_eq!(*list.get(8), "eighty" );
+	assert_eq!(*list.get(9), "ninety" );
+}
+
+
+fn test_insert_at_beginning() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.insert(0, "Sunday");
+	list.insert(0, "Monday");
+	list.insert(0, "Tuesday");
+	assert_eq!(list.len(), 3);
+	assert_eq!(*list.get(0), "Tuesday");
+	assert_eq!(*list.get(1), "Monday" );
+	assert_eq!(*list.get(2), "Sunday" );
+}
+
+
+fn test_insert_at_end() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.insert(0, "Saturday");
+	list.insert(1, "Friday");
+	list.insert(2, "Thursday");
+	list.insert(3, "Wednesday");
+	assert_eq!(list.len(), 4);
+	assert_eq!(*list.get(0), "Saturday" );
+	assert_eq!(*list.get(1), "Friday"   );
+	assert_eq!(*list.get(2), "Thursday" );
+	assert_eq!(*list.get(3), "Wednesday");
+}
+
+
+fn test_insert_at_middle() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.insert(0, "Up");
+	list.insert(1, "Down");
+	list.insert(1, "Left");
+	list.insert(2, "Right");
+	list.insert(1, "Front");
+	list.insert(2, "Back");
+	assert_eq!(list.len(), 6);
+	assert_eq!(*list.get(0), "Up"   );
+	assert_eq!(*list.get(1), "Front");
+	assert_eq!(*list.get(2), "Back" );
+	assert_eq!(*list.get(3), "Left" );
+	assert_eq!(*list.get(4), "Right");
+	assert_eq!(*list.get(5), "Down" );
+}
+
+
+fn test_insert_list() {
+	let mut list = AvlTreeList::<&'static str>::new();
+	list.append_at(0, &mut vec!["1", "2", "3", "5"]);
+	list.append_at(4, &mut vec!["377", "610", "987"]);
+	list.append_at(4, &mut vec!["8", "13", "21", "144", "233"]);
+	list.append_at(7, &mut vec!["34", "55", "89"]);
+	assert_eq!(list.len(), 15);
+	assert_eq!(*list.get( 0),  "1");
+	assert_eq!(*list.get( 1),  "2");
+	assert_eq!(*list.get( 2),  "3");
+	assert_eq!(*list.get( 3),  "5");
+	assert_eq!(*list.get( 4),  "8");
+	assert_eq!(*list.get( 5), "13");
+	assert_eq!(*list.get( 6), "21");
+	assert_eq!(*list.get( 7), "34");
+	assert_eq!(*list.get( 8), "55");
+	assert_eq!(*list.get( 9), "89");
+	assert_eq!(*list.get(10), "144");
+	assert_eq!(*list.get(11), "233");
+	assert_eq!(*list.get(12), "377");
+	assert_eq!(*list.get(13), "610");
+	assert_eq!(*list.get(14), "987");
+}
+
+
+// Stresses the self-balancing mechanism
+fn test_insert_many_beginning() {
+	let n: i32 = 300000;
+	let mut list = AvlTreeList::<i32>::new();
+	for i in 0 .. n {
+		list.push(i);
+	}
+	
+	for i in 0 .. n {
+		assert_eq!(*list.get(i as usize), i);
+	}
+}
+
+
+// Stresses the self-balancing mechanism
+fn test_insert_many_end() {
+	let n: i32 = 300000;
+	let mut list = AvlTreeList::<i32>::new();
+	for i in (0 .. n).rev() {
+		list.insert(0, i);
+	}
+	
+	for i in 0 .. n {
+		assert_eq!(*list.get(i as usize), i);
+	}
+}
+
+
+// Adds in a weird binary pattern to stress arrays and linked lists
+fn test_insert_many_everywhere() {
+	let n: i32 = 18;
+	let mut list = AvlTreeList::<i32>::new();
+	list.push(0);
+	for i in (0 .. n).rev() {
+		let mut j: i32 = 1 << i;
+		let mut k: usize = 1;
+		while j < (1 << n) {
+			list.insert(k, j);
+			j += 2 << i;
+			k += 2;
+		}
+	}
+	
+	for i in 0 .. list.len() {
+		assert_eq!(*list.get(i), i as i32);
+	}
+}
+
+
+fn test_remove() {
+	let mut list = AvlTreeList::<char>::new();
+	let s = "the quick brown fox jumped over the lazy dog";
+	for c in s.chars() {
+		list.push(c);
+	}
+	assert_eq!(list.len(), s.len());
+	
+	assert_eq!(list.remove( 2), 'e');
+	assert_eq!(list.remove( 4), 'u');
+	assert_eq!(list.remove( 3), 'q');
+	assert_eq!(list.remove( 2), ' ');
+	assert_eq!(list.remove(12), 'f');
+	assert_eq!(list.remove(11), ' ');
+	assert_eq!(list.remove(10), 'n');
+	assert_eq!(list.remove( 9), 'w');
+	assert_eq!(list.remove(11), ' ');
+	assert_eq!(list.remove(11), 'j');
+	assert_eq!(list.remove(11), 'u');
+	assert_eq!(list.remove(10), 'x');
+	assert_eq!(list.remove(11), 'p');
+	assert_eq!(list.remove(12), 'd');
+	assert_eq!(list.remove(11), 'e');
+	assert_eq!(list.remove(13), 'v');
+	assert_eq!(list.remove(13), 'e');
+	assert_eq!(list.remove(19), 'l');
+	assert_eq!(list.remove(20), 'z');
+	assert_eq!(list.remove(19), 'a');
+	assert_eq!(list.remove(18), ' ');
+	assert_eq!(list.remove(22), 'g');
+	
+	let s = "thick broom or they do";
+	assert_eq!(list.len(), s.len());
+	let mut i: usize = 0;
+	for c in s.chars() {
+		assert_eq!(*list.get(i), c);
+		i += 1;
+	}
+	
+	assert_eq!(list.remove(0), 't');
+	assert_eq!(list.remove(2), 'c');
+	assert_eq!(list.remove(2), 'k');
+	assert_eq!(list.remove(2), ' ');
+	assert_eq!(list.remove(2), 'b');
+	assert_eq!(list.remove(2), 'r');
+	assert_eq!(list.remove(2), 'o');
+	assert_eq!(list.remove(2), 'o');
+	assert_eq!(list.remove(4), 'o');
+	assert_eq!(list.remove(7), 'h');
+	assert_eq!(list.remove(5), ' ');
+	assert_eq!(list.remove(5), 't');
+	assert_eq!(list.remove(9), 'o');
+	assert_eq!(list.remove(7), ' ');
+	assert_eq!(list.remove(6), 'y');
+	
+	let s = "him red";
+	let mut i: usize = 0;
+	for c in s.chars() {
+		assert_eq!(*list.get(i), c);
+		i += 1;
+	}
+}
+
+
+fn test_clear() {
+	let mut list = AvlTreeList::<i32>::new();
+	for i in 0i32 .. 20 {
+		list.push(i * i);
+	}
+	
+	list.clear();
+	assert_eq!(list.len(), 0);
+	
+	list.push(- 1);
+	list.push(- 8);
+	list.push(-27);
+	assert_eq!(list.len(), 3);
+	assert_eq!(*list.get(0), - 1);
+	assert_eq!(*list.get(1), - 8);
+	assert_eq!(*list.get(2), -27);
+}
+
+
+// Comprehensively tests all the defined methods.
+fn test_against_rust_vec_randomly() {
+	let trials = 100_000;
+	let mut rng = rand::thread_rng();
+	let opcountdist = Range::new(1, 101);
+	let valuedist = Range::new(0i32, 1000000i32);
+	
+	let mut list0 = Vec::<i32>::new();
+	let mut list1 = AvlTreeList::<i32>::new();
+	let mut size: usize = 0;
+	for _ in 0 .. trials {
+		let op = Range::new(0, 100).ind_sample(&mut rng);
+		
+		if op < 1 {  // Clear
+			list1.check_structure();
+			list0.clear();
+			list1.clear();
+			size = 0;
+			
+		} else if op < 2 {  // Set
+			if size > 0 {
+				let index = Range::new(0, size).ind_sample(&mut rng);
+				let val = valuedist.ind_sample(&mut rng);
+				list0[index] = val;
+				list1.set(index, val);
+			}
+			
+		} else if op < 30 {  // Random insertion
+			let n = opcountdist.ind_sample(&mut rng);
+			for _ in 0 .. n {
+				let index = Range::new(0, size + 1).ind_sample(&mut rng);
+				let val = valuedist.ind_sample(&mut rng);
+				list0.insert(index, val);
+				list1.insert(index, val);
+			}
+			size += n;
+			
+		} else if op < 50 {  // Ascending insertion
+			let n = opcountdist.ind_sample(&mut rng);
+			let offset =  Range::new(0, size + 1).ind_sample(&mut rng);
+			for i in 0 .. n {
+				let val = valuedist.ind_sample(&mut rng);
+				list0.insert(offset + i, val);
+				list1.insert(offset + i, val);
+			}
+			size += n;
+			
+		} else if op < 70 {  // Descending insertion
+			let n = opcountdist.ind_sample(&mut rng);
+			let offset = Range::new(0, size + 1).ind_sample(&mut rng);
+			for _ in 0 .. n {
+				let val = valuedist.ind_sample(&mut rng);
+				list0.insert(offset, val);
+				list1.insert(offset, val);
+			}
+			size += n;
+			
+		} else if op < 80 {  // Random deletion
+			let n = std::cmp::min(opcountdist.ind_sample(&mut rng), size);
+			for _ in 0 .. n {
+				let index = Range::new(0, size).ind_sample(&mut rng);
+				assert_eq!(list0.remove(index), list1.remove(index));
+				size -= 1;
+			}
+			
+		} else if op < 90 {  // Ascending deletion
+			if size > 0 {
+				let offset = Range::new(0, size).ind_sample(&mut rng);
+				let n = std::cmp::min(opcountdist.ind_sample(&mut rng), size - offset);
+				for _ in 0 .. n {
+					assert_eq!(list0.remove(offset), list1.remove(offset));
+				}
+				size -= n;
+			}
+			
+		} else if op < 100 {  // Descending deletion
+			if size > 0 {
+				let offset = Range::new(0, size).ind_sample(&mut rng);
+				let n = std::cmp::min(opcountdist.ind_sample(&mut rng), offset + 1);
+				for i in 0 .. n {
+					assert_eq!(list0.remove(offset - i), list1.remove(offset - i));
+				}
+				size -= n;
+			}
+		} else {
+			panic!("Assertion error");
+		}
+		
+		assert_eq!(list0.len(), size);
+		assert_eq!(list1.len(), size);
+		if size > 0 {
+			let indexdist = Range::new(0, size);
+			for _ in 0 .. 10 {
+				let index = indexdist.ind_sample(&mut rng);
+				assert_eq!(list0[index], *list1.get(index));
+			}
+		}
+	}
+}
---
+++ avl-tree-list/avltreelist.rs
@@ -0,0 +1,385 @@
+/* 
+ * AVL tree list (Rust)
+ * 
+ * Copyright (c) 2017 Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/avl-tree-list
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+use std;
+
+
+pub struct AvlTreeList<E> {
+	root: Node<E>,
+}
+
+
+impl <E> AvlTreeList<E> {
+	
+	pub fn new() -> AvlTreeList<E> {
+		AvlTreeList {
+			root: Node::EmptyLeafNode,
+		}
+	}
+	
+	
+	pub fn len(&self) -> usize {
+		self.root.size()
+	}
+	
+	
+	pub fn get(&self, index: usize) -> &E {
+		assert!(index < self.len(), "Index out of bounds");
+		&self.root.get_at(index)
+	}
+	
+	
+	pub fn set(&mut self, index: usize, val: E) {
+		assert!(index < self.len(), "Index out of bounds");
+		self.root.set_at(index, val);
+	}
+	
+	
+	pub fn push(&mut self, val: E) {
+		let index = self.len();
+		self.insert(index, val);
+	}
+	
+	
+	pub fn append(&mut self, vals: &mut Vec<E>) {
+		let index = self.len();
+		self.append_at(index, vals);
+	}
+	
+	
+	pub fn append_at(&mut self, index: usize, vals: &mut Vec<E>) {
+		loop {
+			match vals.pop() {
+				None => break,
+				Some(val) => self.insert(index, val),
+			}
+		}
+	}
+	
+	
+	pub fn insert(&mut self, index: usize, val: E) {
+		assert!(index <= self.len(), "Index out of bounds");  // Different constraint than the other methods
+		assert!(self.len() < std::usize::MAX, "Maximum size reached");
+		let temp = std::mem::replace(&mut self.root, Node::EmptyLeafNode);
+		self.root = temp.insert_at(index, val);
+	}
+	
+	
+	pub fn remove(&mut self, index: usize) -> E {
+		assert!(index < self.len(), "Index out of bounds");
+		let mut result: Option<E> = None;
+		let temp = std::mem::replace(&mut self.root, Node::EmptyLeafNode);
+		self.root = temp.remove_at(index, &mut result);
+		result.unwrap()
+	}
+	
+	
+	pub fn clear(&mut self) {
+		self.root = Node::EmptyLeafNode;
+	}
+	
+	
+	// For unit tests.
+	pub fn check_structure(&self) {
+		self.root.check_structure();
+	}
+	
+}
+
+
+enum Node<E> {
+	EmptyLeafNode,
+	
+	InternalNode(
+		// The object stored at this node.
+		E,
+		// The height of the tree rooted at this node. Empty nodes have height 0.
+		// This node has height equal to max(left.height, right.height) + 1.
+		i16,
+		// The number of nodes in the tree rooted at this node, including this node.
+		// Empty nodes have size 0. This node has size equal to left.size + right.size + 1.
+		usize,
+		// The root node of the left subtree.
+		Box<Node<E>>,
+		// The root node of the right subtree.
+		Box<Node<E>>),
+}
+
+
+impl <E> Node<E> {
+	
+	fn new(val: E) -> Node<E> {
+		Node::InternalNode(val, 1, 1,
+			Box::new(Node::EmptyLeafNode), Box::new(Node::EmptyLeafNode))
+	}
+	
+	
+	fn height(&self) -> i16 {
+		match *self {
+			Node::EmptyLeafNode => 0,
+			Node::InternalNode(_, ht, _, _, _) => ht,
+		}
+	}
+	
+	
+	fn size(&self) -> usize {
+		match *self {
+			Node::EmptyLeafNode => 0,
+			Node::InternalNode(_, _, sz, _, _) => sz,
+		}
+	}
+	
+	
+	fn get_at(&self, index: usize) -> &E {
+		match *self {
+			Node::EmptyLeafNode => panic!("Assertion error"),
+			Node::InternalNode(ref value, _, size, ref left, ref right) => {
+				assert!(index < size, "Assertion error");
+				let leftsize = left.size();
+				if index < leftsize {
+					left.get_at(index)
+				} else if index > leftsize {
+					right.get_at(index - leftsize - 1)
+				} else {
+					&value
+				}
+			},
+		}
+	}
+	
+	
+	fn set_at(&mut self, index: usize, val: E) {
+		match *self {
+			Node::EmptyLeafNode => panic!("Assertion error"),
+			Node::InternalNode(ref mut value, _, size, ref mut left, ref mut right) => {
+				assert!(index < size, "Assertion error");
+				let leftsize = left.size();
+				if index < leftsize {
+					left.set_at(index, val)
+				} else if index > leftsize {
+					right.set_at(index - leftsize - 1, val)
+				} else {
+					*value = val;
+				}
+			},
+		};
+	}
+	
+	
+	fn insert_at(mut self, index: usize, val: E) -> Self {
+		assert!(index <= self.size(), "Assertion error");
+		match self {
+			Node::EmptyLeafNode => Node::new(val),
+			Node::InternalNode(_, _, _, _, _) => {
+				if let Node::InternalNode(_, _, _, ref mut left, ref mut right) = self {
+					let leftsize = left.size();
+					if index <= leftsize {
+						let temp = std::mem::replace(left, Box::new(Node::EmptyLeafNode));
+						*left = Box::new(temp.insert_at(index, val));
+					} else {
+						let temp = std::mem::replace(right, Box::new(Node::EmptyLeafNode));
+						*right = Box::new(temp.insert_at(index - leftsize - 1, val));
+					}
+				}
+				self.recalculate();
+				self.balance()
+			},
+		}
+	}
+	
+	
+	fn remove_at(mut self, index: usize, outval: &mut Option<E>) -> Self {
+		let recursed;
+		if let Node::InternalNode(_, _, _, ref mut left, ref mut right) = self {
+			let leftsize = left.size();
+			if index < leftsize {
+				let temp = std::mem::replace(left, Box::new(Node::EmptyLeafNode));
+				*left = Box::new(temp.remove_at(index, outval));
+				recursed = true;
+			} else if index > leftsize {
+				let temp = std::mem::replace(right, Box::new(Node::EmptyLeafNode));
+				*right = Box::new(temp.remove_at(index - leftsize - 1, outval));
+				recursed = true;
+			} else {
+				recursed = false;
+			}
+		} else {
+			panic!("Index out of bounds");
+		}
+		if recursed {
+			self.recalculate();
+			return self.balance();
+		}
+		
+		if let Node::InternalNode(val, _, _, left, right) = self {
+			*outval = Some(val);
+			if left.size() == 0 && right.size() == 0 {
+				return Node::EmptyLeafNode;
+			} else if right.size() == 0 {
+				return *left;
+			} else if left.size() == 0 {
+				return *right;
+			} else {
+				let mut tempval: Option<E> = None;
+				let newright = Box::new(right.remove_at(0, &mut tempval));
+				let mut result = Node::InternalNode(tempval.unwrap(), 0, 0, left, newright);
+				result.recalculate();
+				return result.balance();
+			}
+		} else {
+			panic!("Index out of bounds");
+		}
+	}
+	
+	
+	fn balance(mut self) -> Self {
+		if let Node::EmptyLeafNode = self {
+			panic!("Assertion error");
+		}
+		let bal = self.get_balance();
+		assert!(bal.abs() <= 2, "Assertion error");
+		if bal == -2 {
+			if let Node::InternalNode(_, _, _, ref mut left, _) = self {
+				let childbal = left.get_balance();
+				assert!(childbal.abs() <= 1, "Assertion error");
+				if childbal == 1 {
+					let temp = std::mem::replace(left, Box::new(Node::EmptyLeafNode));
+					*left = Box::new(temp.rotate_left());
+				}
+			} else {
+				panic!("Assertion error");
+			}
+			self = self.rotate_right();
+		} else if bal == 2 {
+			if let Node::InternalNode(_, _, _, _, ref mut right) = self {
+				let childbal = right.get_balance();
+				assert!(childbal.abs() <= 1, "Assertion error");
+				if childbal == -1 {
+					let temp = std::mem::replace(right, Box::new(Node::EmptyLeafNode));
+					*right = Box::new(temp.rotate_right());
+				}
+			} else {
+				panic!("Assertion error");
+			}
+			self = self.rotate_left();
+		}
+		assert!(self.get_balance().abs() <= 1, "Assertion error");
+		self
+	}
+	
+	
+	/* 
+	 *   A            B
+	 *  / \          / \
+	 * 0   B   ->   A   2
+	 *    / \      / \
+	 *   1   2    0   1
+	 */
+	fn rotate_left(mut self) -> Node<E> {
+		let mut root;
+		if let Node::InternalNode(_, _, _, _, ref mut right) = self {
+			root = *std::mem::replace(right, Box::new(Node::EmptyLeafNode));
+			if let Node::InternalNode(_, _, _, ref mut left, _) = root {
+				std::mem::swap(left, right);
+			} else {
+				panic!("Assertion error");
+			}
+		} else {
+			panic!("Assertion error");
+		}
+		self.recalculate();
+		if let Node::InternalNode(_, _, _, ref mut left, _) = root {
+			*left = Box::new(self);
+		} else {
+			panic!("Assertion error");
+		}
+		root.recalculate();
+		root
+	}
+	
+	
+	/* 
+	 *     B          A
+	 *    / \        / \
+	 *   A   2  ->  0   B
+	 *  / \            / \
+	 * 0   1          1   2
+	 */
+	fn rotate_right(mut self) -> Node<E> {
+		let mut root;
+		if let Node::InternalNode(_, _, _, ref mut left, _) = self {
+			root = *std::mem::replace(left, Box::new(Node::EmptyLeafNode));
+			if let Node::InternalNode(_, _, _, _, ref mut right) = root {
+				std::mem::swap(right, left);
+			} else {
+				panic!("Assertion error");
+			}
+		} else {
+			panic!("Assertion error");
+		}
+		self.recalculate();
+		if let Node::InternalNode(_, _, _, _, ref mut right) = root {
+			*right = Box::new(self);
+		} else {
+			panic!("Assertion error");
+		}
+		root.recalculate();
+		root
+	}
+	
+	
+	fn recalculate(&mut self) {
+		match *self {
+			Node::EmptyLeafNode => panic!("Assertion error"),
+			Node::InternalNode(_, ref mut height, ref mut size, ref left, ref right) => {
+				assert!(left.height() >= 0 && right.height() >= 0, "Assertion error");
+				*height = std::cmp::max(left.height(), right.height()) + 1;
+				*size = left.size() + right.size() + 1;
+				assert!(*height >= 0, "Assertion error");
+			}
+		}
+	}
+	
+	
+	fn get_balance(&self) -> i16 {
+		match *self {
+			Node::EmptyLeafNode => 0,
+			Node::InternalNode(_, _, _, ref left, ref right) =>
+				right.height() - left.height(),
+		}
+	}
+	
+	
+	// For unit tests, invokable by AvlTreeList<E>.
+	fn check_structure(&self) {
+		match *self {
+			Node::EmptyLeafNode => (),
+			Node::InternalNode(_, height, size, ref left, ref right) => {
+				assert_eq!(height, std::cmp::max(left.height(), right.height()) + 1, "Assertion error");
+				assert_eq!(size, left.size() + right.size() + 1, "Assertion error");
+				assert!(self.get_balance().abs() <= 1, "Assertion error");
+			},
+		}
+	}
+	
+}
