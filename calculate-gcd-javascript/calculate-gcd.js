2f615a99b087cf15ff08ac0a87bc1e813ecaf01b
Project Nayuki <me@nayuki.io>
2018-10-06 14:30:02 -0400

    Updated pages "Calculate GCD (JavaScript)", "Java Native Interface compared to Python/C API", "Overview of Project Nayuki software licenses".

--- calculate-gcd-javascript/calculate-gcd.js
+++ calculate-gcd-javascript/calculate-gcd.js
@@ -1,183 +1,177 @@
-/* 
- * GCD calculator
- * 
- * Copyright (c) 2017 Project Nayuki
+/*
+ * GCD calculator (compiled from TypeScript)
+ *
+ * Copyright (c) 2018 Project Nayuki
  * All rights reserved. Contact Nayuki for licensing.
  * https://www.nayuki.io/page/calculate-gcd-javascript
  */
-
 "use strict";
-
-
-/*---- Entry points from HTML code ----*/
-
-function calculate() {
-	function setOutput(s) {
-		document.getElementById("output").value = s;
-	}
-	
-	var x = document.getElementById("numberX").value;
-	var y = document.getElementById("numberY").value;
-	if (x == "" || y == "") {
-		setOutput("");
-		return;
-	}
-	try {
-		x = new Uint(x);
-		y = new Uint(y);
-	} catch (e) {
-		setOutput("Not an integer");
-		return;
-	}
-	try {
-		setOutput(gcd(x, y).toString());
-	} catch (e) {
-		setOutput("Internal error");
-	}
-}
-
-
-var randomClicked = 0;
-
-function random() {
-	randomClicked++;
-	var limit = randomClicked / 10;
-	var len = Math.floor(Math.random() * limit) + 1;
-	function genRandom() {
-		var result = "";
-		for (var i = 0; i < len; i++)
-			result += Math.floor(Math.random() * 10);
-		while (result.length > 1 && result.startsWith("0"))
-			result = result.substring(1);
-		return result;
-	}
-	document.getElementById("numberX").value = genRandom();
-	document.getElementById("numberY").value = genRandom();
-	calculate();
-}
-
-
-/*---- Algorithms and structures ----*/
-
-// Returns the GCD of the given Uint objects, using the binary GCD algorithm.
-function gcd(x, y) {
-	var twos = 0;
-	while (true) {
-		if (x.isLessThan(y)) {
-			var temp = x;
-			x = y;
-			y = temp;
-		}
-		if (y.isZero())
-			break;
-		if (x.isEven() && y.isEven()) {
-			x = x.divide2Exact();
-			y = y.divide2Exact();
-			twos++;
-		} else if (x.isEven())
-			x = x.divide2Exact();
-		else if (y.isEven())
-			y = y.divide2Exact();
-		else
-			x = x.subtract(y).divide2Exact();
-	}
-	for (var i = 0; i < twos; i++)
-		x = x.multiply(2);
-	return x;
-}
-
-
-// An unsigned big integer represented in decimal (base 10).
-function Uint(val) {
-	// Constructor
-	if (typeof val == "string") {
-		if (!/^[0-9]+$/.test(val))
-			throw "Invalid number string";
-		this.digits = [];
-		for (var i = 0; i < val.length; i++)
-			this.digits.push(parseInt(val.charAt(i), 10));
-		this.digits.reverse();
-	} else if (Array.isArray(val)) {
-		if (val.length == 0)
-			this.digits = [0];
-		else
-			this.digits = val.slice();
-	} else
-		throw "Invalid argument type";
-	// Remove trailing zeros
-	while (this.digits.length > 1 && this.digits[this.digits.length - 1] == 0)
-		this.digits.pop();
-	
-	// Arithmetic methods
-	
-	this.isZero = function() {
-		for (var i = 0; i < this.digits.length; i++) {
-			if (this.digits[i] != 0)
-				return false;
-		}
-		return true;
-	};
-	
-	this.isEven = function() {
-		return this.digits[0] % 2 == 0;
-	};
-	
-	this.isLessThan = function(other) {
-		var result = false;
-		var a = this.digits;
-		var b = other.digits;
-		for (var i = 0; i < a.length || i < b.length; i++) {
-			var x = i < a.length ? a[i] : 0;
-			var y = i < b.length ? b[i] : 0;
-			if (x < y)
-				result = true;
-			if (x > y)
-				result = false;
-		}
-		return result;
-	};
-	
-	this.subtract = function(other) {
-		var newDigits = [];
-		var borrow = 0;
-		var a = this.digits;
-		var b = other.digits;
-		for (var i = 0; i < a.length || i < b.length; i++) {
-			var x = i < a.length ? a[i] : 0;
-			var y = i < b.length ? b[i] : 0;
-			var diff = x - y - borrow;
-			borrow = -Math.floor(diff / 10);
-			newDigits.push(diff + borrow * 10);
-		}
-		if (borrow > 0)
-			throw "Negative result";
-		return new Uint(newDigits);
-	};
-	
-	// n must be a plain integer in the range [0, 9].
-	this.multiply = function(n) {
-		var newDigits = [];
-		var carry = 0;
-		this.digits.forEach(function(digit) {
-			var sum = digit * n + carry;
-			newDigits.push(sum % 10);
-			carry = Math.floor(sum / 10);
-		});
-		if (carry > 0)
-			newDigits.push(carry);
-		return new Uint(newDigits);
-	};
-	
-	this.divide2Exact = function() {
-		if (!this.isEven())
-			throw "Number is odd";
-		var temp = this.multiply(5);
-		var newDigits = temp.digits.slice();
-		newDigits.shift();
-		return new Uint(newDigits);
-	};
-	
-	this.toString = function() {
-		return this.digits.slice().reverse().map(function(digit) { return digit.toString(); }).join("");
-	};
-}
+var app;
+(function (app) {
+    /*---- Preamble definitions ----*/
+    function inputElem(id) {
+        var result = document.getElementById(id);
+        if (result instanceof HTMLInputElement)
+            return result;
+        throw "Assertion error";
+    }
+    var outputElem = inputElem("output");
+    /*---- Entry points from HTML page ----*/
+    function doCalculate() {
+        var xStr = inputElem("numberX").value;
+        var yStr = inputElem("numberY").value;
+        if (xStr == "" || yStr == "") {
+            outputElem.value = "";
+            return;
+        }
+        var xInt;
+        var yInt;
+        try {
+            xInt = new Uint(xStr);
+            yInt = new Uint(yStr);
+        }
+        catch (e) {
+            outputElem.value = "Not an integer";
+            return;
+        }
+        try {
+            outputElem.value = xInt.gcd(yInt).toString();
+        }
+        catch (e) {
+            outputElem.value = "Assertion error";
+        }
+    }
+    app.doCalculate = doCalculate;
+    var randomClicked = 0;
+    function doRandom() {
+        randomClicked++;
+        var limit = randomClicked / 10;
+        var len = Math.floor(Math.random() * limit) + 1;
+        function genRandom() {
+            var result = "";
+            for (var i = 0; i < len; i++)
+                result += Math.floor(Math.random() * 10);
+            return result.replace(/^0+(.)/g, "$1");
+        }
+        inputElem("numberX").value = genRandom();
+        inputElem("numberY").value = genRandom();
+        doCalculate();
+    }
+    app.doRandom = doRandom;
+    /*---- Data structure and algorithms ----*/
+    // An unsigned big integer represented in decimal (base 10).
+    var Uint = /** @class */ (function () {
+        function Uint(val) {
+            this.digits = []; // Little endian
+            if (typeof val == "string") {
+                if (!/^[0-9]+$/.test(val))
+                    throw "Invalid number string";
+                for (var _i = 0, val_1 = val; _i < val_1.length; _i++) {
+                    var c = val_1[_i];
+                    this.digits.push(parseInt(c, 10));
+                }
+                this.digits.reverse();
+            }
+            else if (Array.isArray(val)) {
+                if (val.length == 0)
+                    this.digits = [0];
+                else
+                    this.digits = val.slice();
+            }
+            else
+                throw "Invalid argument type";
+            // Remove trailing zeros
+            while (this.digits.length > 1 && this.digits[this.digits.length - 1] == 0)
+                this.digits.pop();
+            if (this.digits.length == 0)
+                throw "Assertion error";
+        }
+        Uint.prototype.isZero = function () {
+            return this.digits.every(function (d) { return d == 0; });
+        };
+        Uint.prototype.isEven = function () {
+            return this.digits[0] % 2 == 0;
+        };
+        Uint.prototype.isLessThan = function (other) {
+            var result = false;
+            var a = this.digits;
+            var b = other.digits;
+            for (var i = 0; i < a.length || i < b.length; i++) {
+                var x = i < a.length ? a[i] : 0;
+                var y = i < b.length ? b[i] : 0;
+                if (x < y)
+                    result = true;
+                if (x > y)
+                    result = false;
+            }
+            return result;
+        };
+        Uint.prototype.subtract = function (other) {
+            var newDigits = [];
+            var borrow = 0;
+            var a = this.digits;
+            var b = other.digits;
+            for (var i = 0; i < a.length || i < b.length; i++) {
+                var x = i < a.length ? a[i] : 0;
+                var y = i < b.length ? b[i] : 0;
+                var diff = x - y - borrow;
+                borrow = -Math.floor(diff / 10);
+                newDigits.push(diff + borrow * 10);
+            }
+            if (borrow > 0)
+                throw "Negative result";
+            return new Uint(newDigits);
+        };
+        // n must be in the range [0, 9].
+        Uint.prototype.multiply = function (n) {
+            var newDigits = [];
+            var carry = 0;
+            this.digits.forEach(function (digit) {
+                var sum = digit * n + carry;
+                newDigits.push(sum % 10);
+                carry = Math.floor(sum / 10);
+            });
+            if (carry > 0)
+                newDigits.push(carry);
+            return new Uint(newDigits);
+        };
+        Uint.prototype.divide2Exact = function () {
+            if (!this.isEven())
+                throw "Number is odd";
+            var temp = this.multiply(5);
+            var newDigits = temp.digits.slice();
+            newDigits.shift();
+            return new Uint(newDigits);
+        };
+        Uint.prototype.gcd = function (other) {
+            var _a;
+            var x = this;
+            var y = other;
+            var twos = 0;
+            while (true) { // Binary GCD algorithm
+                if (x.isLessThan(y))
+                    _a = [y, x], x = _a[0], y = _a[1];
+                if (y.isZero())
+                    break;
+                if (x.isEven() && y.isEven()) {
+                    x = x.divide2Exact();
+                    y = y.divide2Exact();
+                    twos++;
+                }
+                else if (x.isEven())
+                    x = x.divide2Exact();
+                else if (y.isEven())
+                    y = y.divide2Exact();
+                else
+                    x = x.subtract(y).divide2Exact();
+            }
+            for (var i = 0; i < twos; i++)
+                x = x.multiply(2);
+            return x;
+        };
+        Uint.prototype.toString = function () {
+            return this.digits.slice().reverse().map(function (d) { return d.toString(); }).join("");
+        };
+        return Uint;
+    }());
+})(app || (app = {}));
---
+++ calculate-gcd-javascript/calculate-gcd.ts
@@ -0,0 +1,204 @@
+/* 
+ * GCD calculator
+ * 
+ * Copyright (c) 2018 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/calculate-gcd-javascript
+ */
+
+"use strict";
+
+
+namespace app {
+	
+	/*---- Preamble definitions ----*/
+	
+	function inputElem(id: string): HTMLInputElement {
+		const result = document.getElementById(id);
+		if (result instanceof HTMLInputElement)
+			return result;
+		throw "Assertion error";
+	}
+	
+	const outputElem = inputElem("output");
+	
+	
+	
+	/*---- Entry points from HTML page ----*/
+	
+	export function doCalculate(): void {
+		let xStr: string = inputElem("numberX").value;
+		let yStr: string = inputElem("numberY").value;
+		if (xStr == "" || yStr == "") {
+			outputElem.value = "";
+			return;
+		}
+		let xInt: Uint;
+		let yInt: Uint;
+		try {
+			xInt = new Uint(xStr);
+			yInt = new Uint(yStr);
+		} catch (e) {
+			outputElem.value = "Not an integer";
+			return;
+		}
+		try {
+			outputElem.value = xInt.gcd(yInt).toString();
+		} catch (e) {
+			outputElem.value = "Assertion error";
+		}
+	}
+	
+	
+	let randomClicked: number = 0;
+	
+	export function doRandom(): void {
+		randomClicked++;
+		let limit: number = randomClicked / 10;
+		let len: number = Math.floor(Math.random() * limit) + 1;
+		function genRandom(): string {
+			let result: string = "";
+			for (let i = 0; i < len; i++)
+				result += Math.floor(Math.random() * 10);
+			return result.replace(/^0+(.)/g, "$1");
+		}
+		inputElem("numberX").value = genRandom();
+		inputElem("numberY").value = genRandom();
+		doCalculate();
+	}
+	
+	
+	
+	/*---- Data structure and algorithms ----*/
+	
+	// An unsigned big integer represented in decimal (base 10).
+	class Uint {
+		
+		private readonly digits: Array<number> = [];  // Little endian
+		
+		
+		public constructor(val: string|Array<number>) {
+			if (typeof val == "string") {
+				if (!/^[0-9]+$/.test(val))
+					throw "Invalid number string";
+				for (let c of val)
+					this.digits.push(parseInt(c, 10));
+				this.digits.reverse();
+			} else if (Array.isArray(val)) {
+				if (val.length == 0)
+					this.digits = [0];
+				else
+					this.digits = val.slice();
+			} else
+				throw "Invalid argument type";
+			
+			// Remove trailing zeros
+			while (this.digits.length > 1 && this.digits[this.digits.length - 1] == 0)
+				this.digits.pop();
+			if (this.digits.length == 0)
+				throw "Assertion error";
+		}
+		
+		
+		public isZero(): boolean {
+			return this.digits.every(d => d == 0);
+		}
+		
+		
+		public isEven(): boolean {
+			return this.digits[0] % 2 == 0;
+		}
+		
+		
+		public isLessThan(other: Uint): boolean {
+			let result: boolean = false;
+			const a: Array<number> = this .digits;
+			const b: Array<number> = other.digits;
+			for (let i = 0; i < a.length || i < b.length; i++) {
+				const x: number = i < a.length ? a[i] : 0;
+				const y: number = i < b.length ? b[i] : 0;
+				if (x < y)
+					result = true;
+				if (x > y)
+					result = false;
+			}
+			return result;
+		}
+		
+		
+		public subtract(other: Uint): Uint {
+			let newDigits: Array<number> = [];
+			let borrow: number = 0;
+			const a: Array<number> = this .digits;
+			const b: Array<number> = other.digits;
+			for (let i = 0; i < a.length || i < b.length; i++) {
+				const x: number = i < a.length ? a[i] : 0;
+				const y: number = i < b.length ? b[i] : 0;
+				const diff: number = x - y - borrow;
+				borrow = -Math.floor(diff / 10);
+				newDigits.push(diff + borrow * 10);
+			}
+			if (borrow > 0)
+				throw "Negative result";
+			return new Uint(newDigits);
+		}
+		
+		
+		// n must be in the range [0, 9].
+		public multiply(n: number): Uint {
+			let newDigits: Array<number> = [];
+			let carry: number = 0;
+			this.digits.forEach(digit => {
+				const sum = digit * n + carry;
+				newDigits.push(sum % 10);
+				carry = Math.floor(sum / 10);
+			});
+			if (carry > 0)
+				newDigits.push(carry);
+			return new Uint(newDigits);
+		}
+		
+		
+		public divide2Exact(): Uint {
+			if (!this.isEven())
+				throw "Number is odd";
+			let temp: Uint = this.multiply(5);
+			let newDigits: Array<number> = temp.digits.slice();
+			newDigits.shift();
+			return new Uint(newDigits);
+		}
+		
+		
+		public gcd(other: Uint): Uint {
+			let x: Uint = this;
+			let y: Uint = other;
+			let twos: number = 0;
+			while (true) {  // Binary GCD algorithm
+				if (x.isLessThan(y))
+					[x, y] = [y, x];
+				if (y.isZero())
+					break;
+				if (x.isEven() && y.isEven()) {
+					x = x.divide2Exact();
+					y = y.divide2Exact();
+					twos++;
+				} else if (x.isEven())
+					x = x.divide2Exact();
+				else if (y.isEven())
+					y = y.divide2Exact();
+				else
+					x = x.subtract(y).divide2Exact();
+			}
+			for (let i = 0; i < twos; i++)
+				x = x.multiply(2);
+			return x;
+		}
+		
+		
+		public toString(): string {
+			return this.digits.slice().reverse().map(d => d.toString()).join("");
+		}
+		
+	}
+	
+}
