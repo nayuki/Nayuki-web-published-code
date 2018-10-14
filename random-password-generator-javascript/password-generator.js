37dfe083823331ae538750a247b875f27ba0f3c7
Project Nayuki <me@nayuki.io>
2018-10-14 01:04:29 -0400

    Updated pages "Random password generator (JavaScript)", "About", "QR Code generator library", "Overview of Project Nayuki software licenses".

--- random-password-generator-javascript/password-generator.js
+++ random-password-generator-javascript/password-generator.js
@@ -9,6 +9,8 @@
 "use strict";
 
 
+/*---- Configuration ----*/
+
 var CHARACTER_SETS = [
 	[true, "Numbers", "0123456789"],
 	[true, "Lowercase", "abcdefghijklmnopqrstuvwxyz"],
@@ -18,113 +20,190 @@ var CHARACTER_SETS = [
 ];
 
 
-var passwordElem = document.getElementById("password");
-var statisticsElem = document.getElementById("statistics");
-statisticsElem.textContent = "\u00A0";
 
-function init() {
-	var elements = document.createDocumentFragment();
+/*---- Global variables ----*/
+
+var passwordElem   = document.getElementById("password"   );
+var statisticsElem = document.getElementById("statistics" );
+var copyElem       = document.getElementById("copy-button")
+var cryptoObject    = null;
+var currentPassword = null;
+
+
+
+/*---- Initialization ----*/
+
+function initCharsets() {
+	function createElem(tagName, attribs) {
+		var result = document.createElement(tagName);
+		if (attribs !== undefined) {
+			for (var key in attribs)
+				result[key] = attribs[key];
+		}
+		return result;
+	}
+	
+	var container = document.querySelector("#charset tbody");
+	var endElem = document.querySelector("#charset tbody > tr:last-child");
 	CHARACTER_SETS.forEach(function(entry, i) {
-		var spanElem = document.createElement("span");
-		var inputElem = document.createElement("input");
-		inputElem.type = "checkbox";
-		inputElem.checked = entry[0];
-		inputElem.id = "charset-" + i;
-		spanElem.appendChild(inputElem);
-		var labelElem = document.createElement("label");
-		labelElem.htmlFor = inputElem.id;
-		labelElem.textContent = " " + entry[1] + " ";
-		var smallElem = document.createElement("small");
-		smallElem.textContent = "(" + entry[2] + ")";
-		labelElem.appendChild(smallElem);
-		spanElem.appendChild(labelElem);
-		elements.appendChild(spanElem);
-		elements.appendChild(document.createElement("br"));
+		var tr = createElem("tr");
+		var td = createElem("td");
+		var input = createElem("input", {
+			type: "checkbox",
+			checked: entry[0],
+			id: "charset-" + i});
+		td.appendChild(input);
+		tr.appendChild(td);
+		var td = createElem("td");
+		var label = createElem("label", {
+			htmlFor: "charset-" + i,
+			textContent: " " + entry[1] + " "});
+		var small = createElem("small", {
+			textContent: "(" + entry[2] + ")"})
+		label.appendChild(small);
+		td.appendChild(label);
+		tr.appendChild(td);
+		container.insertBefore(tr, endElem);
 	});
-	var containerElem = document.getElementById("charset-checkboxes");
-	containerElem.insertBefore(elements, containerElem.firstChild);
 }
 
-init();
+
+function initCrypto() {
+	var elem = document.getElementById("crypto-getrandomvalues-entropy");
+	elem.textContent = "\u2717";  // X mark
+	
+	if ("crypto" in window)
+		cryptoObject = crypto;
+	else if ("msCrypto" in window)
+		cryptoObject = msCrypto;
+	else
+		return;
+	
+	if ("getRandomValues" in cryptoObject && "Uint32Array" in window && typeof Uint32Array == "function")
+		elem.textContent = "\u2713";  // Check mark
+	else
+		cryptoObject = null;
+}
 
 
-// The one and only function called from the HTML code
-function generate() {
-	// Gather the character set
-	var charsetStr = "";
+
+/*---- Entry points from HTML code ----*/
+
+function doGenerate(ev) {
+	ev.preventDefault();
+	
+	// Get and check character set
+	var charset = getPasswordCharacterSet();
+	if (charset.length == 0) {
+		alert("Error: Character set is empty");
+		return;
+	} else if (document.getElementById("by-entropy").checked && charset.length == 1) {
+		alert("Error: Need at least 2 distinct characters in set");
+		return;
+	}
+	
+	// Calculate desired length
+	var length;
+	if (document.getElementById("by-length").checked)
+		length = parseInt(document.getElementById("length").value, 10);
+	else if (document.getElementById("by-entropy").checked)
+		length = Math.ceil(parseFloat(document.getElementById("entropy").value) * Math.log(2) / Math.log(charset.length));
+	else
+		throw "Assertion error";
+	
+	// Check length
+	if (length < 0) {
+		alert("Negative password length");
+		return;
+	} else if (length > 10000) {
+		alert("Password length too large");
+		return;
+	}
+	
+	// Generate password
+	currentPassword = generatePassword(charset, length);
+	
+	// Calculate and format entropy
+	var entropy = Math.log(charset.length) * length / Math.log(2);
+	var entropystr;
+	if (entropy < 70)
+		entropystr = entropy.toFixed(2);
+	else if (entropy < 200)
+		entropystr = entropy.toFixed(1);
+	else
+		entropystr = entropy.toFixed(0);
+	
+	// Set output elements
+	passwordElem.textContent = currentPassword;
+	statisticsElem.textContent = "Length = " + length + " chars, \u00A0\u00A0Charset size = " +
+		charset.length + " symbols, \u00A0\u00A0Entropy = " + entropystr + " bits";
+	copyElem.disabled = false;
+}
+
+
+function doCopy() {
+	var container = document.querySelector("article");
+	var textarea = document.createElement("textarea");
+	textarea.style.position = "fixed";
+	textarea.style.opacity = "0";
+	container.insertBefore(textarea, container.firstChild);
+	textarea.value = currentPassword;
+	textarea.focus();
+	textarea.select();
+	document.execCommand("copy");
+	container.removeChild(textarea);
+}
+
+
+
+/*---- Low-level functions ----*/
+
+function getPasswordCharacterSet() {
+	// Concatenate characters from every checked entry
+	var rawCharset = "";
 	CHARACTER_SETS.forEach(function(entry, i) {
 		if (document.getElementById("charset-" + i).checked)
-			charsetStr += entry[2];
+			rawCharset += entry[2];
 	});
 	if (document.getElementById("custom").checked)
-		charsetStr += document.getElementById("customchars").value;
-	charsetStr = charsetStr.replace(/ /, "\u00A0");  // Replace space with non-breaking space
+		rawCharset += document.getElementById("customchars").value;
+	rawCharset = rawCharset.replace(/ /g, "\u00A0");  // Replace space with non-breaking space
 	
-	// Convert to array and remove duplicate characters
+	// Parse UTF-16, remove duplicates, convert to array of strings
 	var charset = [];
-	for (var i = 0; i < charsetStr.length; i++) {
-		var c = charsetStr.charCodeAt(i);
-		var s = null;
-		if (c < 0xD800 || c >= 0xE000)  // Regular UTF-16 character
-			s = charsetStr.charAt(i);
-		else if (0xD800 <= c && c < 0xDC00) {  // High surrogate
-			if (i + 1 < charsetStr.length) {
-				var d = charsetStr.charCodeAt(i + 1);
-				if (0xDC00 <= d && d < 0xE000) {
-					// Valid character in supplementary plane
-					s = charsetStr.substr(i, 2);
-					i++;
-				}
-				// Else discard unpaired surrogate
-			}
-		} else if (0xDC00 <= d && d < 0xE000)  // Low surrogate
-			i++;  // Discard unpaired surrogate
-		else
-			throw "Assertion error";
-		if (s != null && charset.indexOf(s) == -1)
-			charset.push(s);
-	}
-	
-	var password = "";
-	var statistics = "";
-	if (charset.length == 0)
-		alert("Error: Character set is empty");
-	else if (document.getElementById("by-entropy").checked && charset.length == 1)
-		alert("Error: Need at least 2 distinct characters in set");
-	else {
-		var length;
-		if (document.getElementById("by-length").checked)
-			length = parseInt(document.getElementById("length").value, 10);
-		else if (document.getElementById("by-entropy").checked)
-			length = Math.ceil(parseFloat(document.getElementById("entropy").value) * Math.log(2) / Math.log(charset.length));
-		else
-			throw "Assertion error";
-		
-		if (length < 0)
-			alert("Negative password length");
-		else if (length > 10000)
-			alert("Password length too large");
-		else {
-			for (var i = 0; i < length; i++)
-				password += charset[randomInt(charset.length)];
-			
-			var entropy = Math.log(charset.length) * length / Math.log(2);
-			var entropystr;
-			if (entropy < 70)
-				entropystr = entropy.toFixed(2);
-			else if (entropy < 200)
-				entropystr = entropy.toFixed(1);
-			else
-				entropystr = entropy.toFixed(0);
-			statistics = "Length = " + length + " chars, \u00A0\u00A0Charset size = " + charset.length + " symbols, \u00A0\u00A0Entropy = " + entropystr + " bits";
+	for (var i = 0; i < rawCharset.length; i++) {
+		var c = rawCharset.charCodeAt(i);
+		if (c < 0xD800 || c >= 0xE000) {  // Regular UTF-16 character
+			var s = rawCharset.charAt(i);
+			if (charset.indexOf(s) == -1)
+				charset.push(s);
+			continue;
 		}
+		if (0xD800 <= c && c < 0xDC00 && i + 1 < rawCharset.length) {  // High surrogate
+			var d = rawCharset.charCodeAt(i + 1);
+			if (0xDC00 <= d && d < 0xE000) {  // Low surrogate
+				var s = rawCharset.substr(i, 2);
+				i++;
+				if (charset.indexOf(s) == -1)
+					charset.push(s);
+				continue;
+			}
+		}
+		throw "Invalid UTF-16";
 	}
-	passwordElem.textContent = password;
-	statisticsElem.textContent = statistics;
+	return charset;
 }
 
 
-// Returns a random integer in the range [0, n) using a variety of methods
+function generatePassword(charset, len) {
+	var result = "";
+	for (var i = 0; i < len; i++)
+		result += charset[randomInt(charset.length)];
+	return result;
+}
+
+
+// Returns a random integer in the range [0, n) using a variety of methods.
 function randomInt(n) {
 	var x = randomIntMathRandom(n);
 	x = (x + randomIntBrowserCrypto(n)) % n;
@@ -132,7 +211,7 @@ function randomInt(n) {
 }
 
 
-// Not secure or high quality, but always available
+// Not secure or high quality, but always available.
 function randomIntMathRandom(n) {
 	var x = Math.floor(Math.random() * n);
 	if (x < 0 || x >= n)
@@ -141,9 +220,7 @@ function randomIntMathRandom(n) {
 }
 
 
-var cryptoObject = null;
-
-// Uses a secure, unpredictable random number generator if available; otherwise returns 0
+// Uses a secure, unpredictable random number generator if available; otherwise returns 0.
 function randomIntBrowserCrypto(n) {
 	if (cryptoObject == null)
 		return 0;
@@ -155,22 +232,9 @@ function randomIntBrowserCrypto(n) {
 }
 
 
-function initCrypto() {
-	var elem = document.getElementById("crypto-getrandomvalues-entropy");
-	elem.textContent = "\u2717";
-	
-	if ("crypto" in window)
-		cryptoObject = crypto;
-	else if ("msCrypto" in window)
-		cryptoObject = msCrypto;
-	else
-		return;
-	
-	if ("getRandomValues" in cryptoObject && "Uint32Array" in window && typeof Uint32Array == "function")
-		elem.textContent = "\u2713";
-	else
-		cryptoObject = null;
-}
 
+/*---- Initialization ----*/
 
+initCharsets();
 initCrypto();
+copyElem.disabled = true;
