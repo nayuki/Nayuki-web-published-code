8588bdea76c944e0e44244495483d44445324cc4
Project Nayuki <me@nayuki.io>
2018-11-12 03:06:05 +0000

    Updated code on page "GIF optimizer (Java)".

--- gif-optimizer-java/GifLzwDecompressor.java
+++ gif-optimizer-java/GifLzwDecompressor.java
@@ -13,100 +13,139 @@ import java.util.Arrays;
 import java.util.Objects;
 
 
-// Decompresses GIF's dialect of LZW-encoded data. Requires prior unpacking of GIF subblocks.
+// Decompresses GIF's dialect of LZW-encoded data. Requires prior unpacking of GIF data subblocks.
 final class GifLzwDecompressor {
 	
-	// Static method for decompression.
 	public static byte[] decode(BitInputStream in, int codeBits) throws IOException {
+		// Check arguments
+		Objects.requireNonNull(in);
+		if (!(2 <= codeBits && codeBits <= 8))
+			throw new IllegalArgumentException();
+		
+		// Initialize dictionary
+		LzwDictionary dict = new LzwDictionary(1 << codeBits);
+		final int clearCode = dict.alphabetSize;
+		final int stopCode = clearCode + 1;
+		
+		// Start decoding
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		new GifLzwDecompressor(in, 1 << codeBits, out);
+		for (int prevSymbol = -1; ; ) {  // Parse and process each symbol
+			int symbol = in.readBits(dict.currentCodeBits);
+			if (symbol >= dict.currentSize)
+				throw new IllegalArgumentException("Symbol out of range");
+			else if (symbol == stopCode)
+				break;
+			else if (symbol == clearCode) {
+				dict.resetEntries();
+				prevSymbol = -1;
+			} else {  // Ordinary data symbol
+				// Add new entry to dictionary if not at beginning and not full
+				if (prevSymbol != -1)
+					dict.fillPlaceholderEntry(prevSymbol, symbol);
+				
+				// Write bytes of the current symbol's entry
+				dict.writeEntry(symbol, out);
+				prevSymbol = symbol;
+				
+				// Increment size before knowing what the encoder just added
+				dict.addPlaceholderEntry();
+			}
+		}
 		return out.toByteArray();
 	}
 	
 	
 	
-	/* Stateful decompressor object */
-	
-	private static final int MAX_DICT_SIZE = 4096;
-	
-	private final int alphabetSize;
-	private final int[] symbolStart;
-	private int curDictSize;
-	private int curCodeBits;
-	
-	
-	private GifLzwDecompressor(BitInputStream in, int alphaSize, OutputStream out) throws IOException {
-		Objects.requireNonNull(in);
+	private static final class LzwDictionary {
 		
-		// Initialize dictionary with literals
-		symbolStart = new int[MAX_DICT_SIZE + 1];
-		byte[] dictionary = new byte[alphaSize];  // Minimum initial size
-		for (int i = 0; i < alphaSize; i++) {
-			symbolStart[i] = i;
-			dictionary[i] = (byte)i;
+		public final int alphabetSize;
+		
+		public int currentSize;
+		public int currentCodeBits;
+		
+		private byte[] symbolBytes;
+		private int[] symbolStarts;
+		
+		
+		public LzwDictionary(int alphaSz) {
+			if (!(1 <= alphaSz && alphaSz <= 256))
+				throw new IllegalArgumentException();
+			alphabetSize = alphaSz;
+			
+			// Fill prefix with literals
+			symbolStarts = new int[MAX_ENTRIES + 1];
+			symbolBytes = new byte[alphaSz];  // Minimum initial size
+			for (int i = 0; i < alphaSz; i++) {
+				symbolStarts[i] = i;
+				symbolBytes[i] = (byte)i;
+			}
+			
+			// Add special zero-length Clear and Stop symbols
+			for (int i = 0; i <= 2; i++)
+				symbolStarts[alphaSz + i] = alphaSz;
+			
+			resetEntries();
 		}
 		
-		// Clear and Stop symbols shall be zero-length
-		for (int i = 0; i <= 2; i++)
-			symbolStart[alphaSize + i] = alphaSize;
 		
-		// Initialize other fields
-		alphabetSize = alphaSize;
-		clearDictionary();
+		public void resetEntries() {
+			currentSize = alphabetSize + 2;  // Alphabet plus Clear and Stop symbols
+			currentCodeBits = 32 - Integer.numberOfLeadingZeros(currentSize - 1);  // ceil(log2(currentSize))
+			Arrays.fill(symbolStarts, currentSize + 1, symbolStarts.length, -1);  // Mark unused entries
+		}
 		
-		// Start decoding
-		final int clearCode = alphaSize;
-		final int stopCode = clearCode + 1;
-		int prevSymbol = -1;
-		while (true) {
-			// Process next symbol
-			int symbol = in.readBits(curCodeBits);
-			if (symbol >= curDictSize)
-				throw new IllegalArgumentException("Symbol out of range");
-			else if (symbol == stopCode)
-				break;
-			else if (symbol == clearCode) {
-				clearDictionary();
-				prevSymbol = -1;
+		
+		public void fillPlaceholderEntry(int prevSymbol, int curSymbol) {
+			// Check arguments
+			if (symbolStarts[currentSize] != -1) {
+				if (currentSize >= MAX_ENTRIES)
+					return;
+				else
+					throw new IllegalStateException();
 			}
-			else {
-				// Add new entry to dictionary
-				if (prevSymbol != -1 && symbolStart[MAX_DICT_SIZE] == -1) {
-					int newSym = curDictSize - 1;
-					int len = symbolStart[prevSymbol + 1] - symbolStart[prevSymbol] + 1;
-					int newStart = symbolStart[newSym];
-					while (newStart + len > dictionary.length)  // Ensure capacity
-						dictionary = Arrays.copyOf(dictionary, dictionary.length * 2);
-					System.arraycopy(dictionary, symbolStart[prevSymbol], dictionary, newStart, len - 1);
-					dictionary[newStart + len - 1] = symbol < newSym ? dictionary[symbolStart[symbol]] : dictionary[symbolStart[prevSymbol]];
-					symbolStart[newSym + 1] = newStart + len;
-				}
-				
-				int start = symbolStart[symbol];
-				int end = symbolStart[symbol + 1];
-				out.write(dictionary, start, end - start);
-				prevSymbol = symbol;
-				
-				if (curDictSize < MAX_DICT_SIZE) {
-					// Increment the dictionary size without yet knowing what the encoder just added
-					if ((curDictSize & (curDictSize - 1)) == 0)  // Is power of 2
-						curCodeBits++;
-					curDictSize++;
-				}
+			if (!(0 <= prevSymbol && prevSymbol < currentSize - 1))
+				throw new IllegalArgumentException();
+			if (!(0 <= curSymbol && curSymbol < currentSize))
+				throw new IllegalArgumentException();
+			
+			// Compute numbers
+			int newEntryLen = symbolStarts[prevSymbol + 1] - symbolStarts[prevSymbol] + 1;
+			int newSymbol = currentSize - 1;
+			int newStart = symbolStarts[newSymbol];
+			int newEnd = newStart + newEntryLen;
+			
+			// Update arrays
+			while (symbolBytes.length < newEnd)  // Ensure capacity
+				symbolBytes = Arrays.copyOf(symbolBytes, symbolBytes.length * 2);
+			System.arraycopy(symbolBytes, symbolStarts[prevSymbol], symbolBytes, newStart, newEntryLen - 1);
+			symbolBytes[newEnd - 1] = symbolBytes[symbolStarts[curSymbol]];
+			symbolStarts[newSymbol + 1] = newEnd;
+		}
+		
+		
+		public void writeEntry(int symbol, OutputStream out) throws IOException {
+			if (!(0 <= symbol && symbol < currentSize))
+				throw new IllegalArgumentException();
+			Objects.requireNonNull(out);
+			int start = symbolStarts[symbol];
+			int end = symbolStarts[symbol + 1];
+			out.write(symbolBytes, start, end - start);
+		}
+		
+		
+		public void addPlaceholderEntry() {
+			if (currentSize < MAX_ENTRIES) {
+				if (symbolStarts[currentSize] == -1)
+					throw new IllegalStateException();
+				if (Integer.bitCount(currentSize) == 1)  // Is power of 2
+					currentCodeBits++;
+				currentSize++;
 			}
 		}
-	}
-	
-	
-	private void clearDictionary() {
-		// Calculate code bits
-		curDictSize = alphabetSize + 2;  // Plus Clear and Stop symbols
-		curCodeBits = 0;  // Always equal to ceil(log2(curDictSize))
-		while ((1 << curCodeBits) < curDictSize)
-			curCodeBits++;
 		
-		// Erase indexes to dictionary entries
-		Arrays.fill(symbolStart, curDictSize + 1, symbolStart.length, -1);
+		
+		public static final int MAX_ENTRIES = 4096;
+		
 	}
 	
 }
