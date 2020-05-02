0e917216c89296b50ab7750a9ebad95165bb1a0f
Project Nayuki <me@nayuki.io>
2020-05-02 23:16:02 +0000

    Updated pgaes "How to implement the discrete Fourier transform", "Poor feedback from readers".

--- how-to-implement-the-discrete-fourier-transform/dft.c
+++ how-to-implement-the-discrete-fourier-transform/dft.c
@@ -1,6 +1,6 @@
 /* 
  * Discrete Fourier transform (C)
- * by Project Nayuki, 2017. Public domain.
+ * by Project Nayuki, 2020. Public domain.
  * https://www.nayuki.io/page/how-to-implement-the-discrete-fourier-transform
  */
 
@@ -11,10 +11,11 @@
  */
 #include <complex.h>
 #include <math.h>
-void compute_dft_complex(const double complex input[], double complex output[], int n) {
-	for (int k = 0; k < n; k++) {  // For each output element
-		complex double sum = 0.0;
-		for (int t = 0; t < n; t++) {  // For each input element
+#include <stddef.h>
+void compute_dft_complex(const double complex input[], double complex output[], size_t n) {
+	for (size_t k = 0; k < n; k++) {  // For each output element
+		double complex sum = 0.0;
+		for (size_t t = 0; t < n; t++) {  // For each input element
 			double angle = 2 * M_PI * t * k / n;
 			sum += input[t] * cexp(-angle * I);
 		}
@@ -29,13 +30,14 @@ void compute_dft_complex(const double complex input[], double complex output[], 
  * All the array arguments must be non-NULL and have a length equal to n.
  */
 #include <math.h>
+#include <stddef.h>
 void compute_dft_real_pair(const double inreal[], const double inimag[],
-		double outreal[], double outimag[], int n) {
+		double outreal[], double outimag[], size_t n) {
 	
-	for (int k = 0; k < n; k++) {  // For each output element
+	for (size_t k = 0; k < n; k++) {  // For each output element
 		double sumreal = 0;
 		double sumimag = 0;
-		for (int t = 0; t < n; t++) {  // For each input element
+		for (size_t t = 0; t < n; t++) {  // For each input element
 			double angle = 2 * M_PI * t * k / n;
 			sumreal +=  inreal[t] * cos(angle) + inimag[t] * sin(angle);
 			sumimag += -inreal[t] * sin(angle) + inimag[t] * cos(angle);
