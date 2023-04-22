c38b4a145e280c9237987fbf54e38b8488e1f539
Project Nayuki <me@nayuki.io>
2023-04-22 17:21:17 +0000

    Updated pages "Knuth's -yllion number notation", "Overview of Project Nayuki software licenses".

---
+++ knuths-yllion-number-notation/integer-to-words.js
@@ -0,0 +1,183 @@
+/*
+ * Knuth's -yllion number notation demo (compiled from TypeScript)
+ *
+ * Copyright (c) 2023 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/knuths-yllion-number-notation
+ */
+"use strict";
+var app;
+(function (app) {
+    let numberInput = document.querySelector("article input#number");
+    let conventionalEnglishGroupingOutput = document.querySelector("article output#conventional-english-grouping");
+    let conventionalEnglishNotationOutput = document.querySelector("article output#conventional-english-notation");
+    let yllionGroupingOutput = document.querySelector("article output#yllion-grouping              ");
+    let yllionEnglishOutput = document.querySelector("article output#yllion-english               ");
+    let yllionChineseOutput = document.querySelector("article output#yllion-chinese               ");
+    function update() {
+        conventionalEnglishGroupingOutput.textContent = "";
+        conventionalEnglishNotationOutput.textContent = "";
+        yllionGroupingOutput.textContent = "";
+        yllionEnglishOutput.textContent = "";
+        yllionChineseOutput.textContent = "";
+        const number = BigInt(numberInput.value);
+        conventionalEnglishGroupingOutput.textContent = ConventionalEnglishNotation.toStringWithCommas(number);
+        yllionGroupingOutput.textContent = YllionEnglishNotation.toStringWithSeparators(number);
+        yllionEnglishOutput.textContent = YllionEnglishNotation.numberToWords(number);
+        yllionChineseOutput.textContent = YllionChineseNotation.numberToWords(number);
+        conventionalEnglishNotationOutput.textContent = ConventionalEnglishNotation.numberToWords(number);
+    }
+    setTimeout(update);
+    numberInput.oninput = update;
+    /*---- Submodules for different number formats ----*/
+    // See https://en.wikipedia.org/wiki/English_numerals .
+    class ConventionalEnglishNotation {
+        // For example: numberToWords(1234567) -> "one million two hundred thirty-four thousand five hundred sixty-seven".
+        static numberToWords(n) {
+            // Simple cases
+            if (n < 0n)
+                return "negative " + ConventionalEnglishNotation.numberToWords(-n);
+            else if (n == 0n)
+                return "zero";
+            // 1 <= n <= 999
+            else if (n < 1000n) {
+                let s = "";
+                if (n >= 100n) {
+                    s += ConventionalEnglishNotation.ONES[Number(n / 100n)] + " hundred";
+                    if (n % 100n != 0n)
+                        s += " ";
+                    n %= 100n;
+                }
+                s += ConventionalEnglishNotation.TENS[Number(n / 10n)];
+                if (n < 20n)
+                    s += ConventionalEnglishNotation.ONES[Number(n)];
+                else if (n % 10n != 0n)
+                    s += "-" + ConventionalEnglishNotation.ONES[Number(n % 10n)];
+                return s;
+            }
+            else { // n >= 1000
+                let parts = [];
+                for (const illion of ConventionalEnglishNotation.ILLIONS) {
+                    if (n == 0n)
+                        break;
+                    const rem = n % 1000n;
+                    if (rem > 0n)
+                        parts.push(ConventionalEnglishNotation.numberToWords(rem) + (illion != "" ? " " + illion : ""));
+                    n /= 1000n;
+                }
+                if (n != 0n)
+                    throw new Error("Number too large");
+                parts.reverse();
+                return parts.join(" ");
+            }
+        }
+        // For example: toStringWithCommas(-123456789) -> "-123,456,798".
+        static toStringWithCommas(n) {
+            if (n < 0n)
+                return "-" + ConventionalEnglishNotation.toStringWithCommas(-n);
+            else {
+                let s = n.toString();
+                for (let i = s.length - 3; i > 0; i -= 3)
+                    s = s.substring(0, i) + "," + s.substring(i);
+                return s;
+            }
+        }
+    }
+    ConventionalEnglishNotation.ONES = [
+        "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
+    ];
+    ConventionalEnglishNotation.TENS = [
+        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
+    ConventionalEnglishNotation.ILLIONS = [
+        "", "thousand", "million", "billion", "trillion", "quadrillion",
+        "quintillion", "sextillion", "septillion", "octillion", "nonillion",
+        "decillion", "undecillion", "duodecillion", "tredecillion", "quattuordecillion",
+        "quindecillion", "sexdecillion", "septendecillion", "octodecillion", "novemdecillion",
+        "vigintillion"
+    ];
+    // Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+    class YllionEnglishNotation {
+        static numberToWords(n) {
+            if (n < 0n)
+                return "negative " + YllionChineseNotation.numberToWords(-n);
+            // 0 <= n <= 99, borrow functionality from another class
+            else if (n < 100n)
+                return ConventionalEnglishNotation.numberToWords(n);
+            else { // n >= 100
+                const temp = n.toString();
+                const yllionsLen = YllionEnglishNotation.YLLIONS.length;
+                if (temp.length > (1 << yllionsLen))
+                    throw new RangeError("Number too large");
+                for (let i = yllionsLen - 1; i >= 1; i--) {
+                    if (temp.length > (1 << i)) {
+                        const split = temp.length - (1 << i);
+                        const high = BigInt(temp.substring(0, split));
+                        const low = BigInt(temp.substring(split));
+                        return (high > 0n ? YllionEnglishNotation.numberToWords(high) + " " + YllionEnglishNotation.YLLIONS[i] : "") +
+                            (high > 0n && low > 0n ? " " : "") +
+                            (low > 0n ? YllionEnglishNotation.numberToWords(low) : "");
+                    }
+                }
+                throw new Error("Unreachable value");
+            }
+        }
+        // For example: toStringWithSeparators(12345678901234567890) -> "1234:5678,9012;3456,7890".
+        static toStringWithSeparators(n) {
+            if (n < 0n)
+                return "-" + YllionEnglishNotation.toStringWithSeparators(-n);
+            else {
+                let s = n.toString();
+                for (let i = s.length - 4, j = 1; i > 0; i -= 4, j++) {
+                    const k = Math.min(YllionEnglishNotation.numTrailingZeros(j), YllionEnglishNotation.SEPARATORS.length - 1);
+                    s = s.substring(0, i) + YllionEnglishNotation.SEPARATORS[k] + s.substring(i);
+                }
+                return s;
+            }
+        }
+        static numTrailingZeros(n) {
+            if (n == 0 || Math.floor(n) != n)
+                throw new RangeError();
+            let result = 0;
+            for (; (n & 1) == 0; n >>>= 1)
+                result++;
+            return result;
+        }
+    }
+    YllionEnglishNotation.YLLIONS = [
+        "", "hundred", "myriad", "myllion", "byllion", "tryllion", "quadryllion",
+        "quintyllion", "sextyllion", "septyllion", "octyllion", "nonyllion", "decyllion"
+    ];
+    YllionEnglishNotation.SEPARATORS = [",", ";", ":", "'"];
+    // Donald Knuth's system; see https://en.wikipedia.org/wiki/-yllion .
+    class YllionChineseNotation {
+        static numberToWords(n) {
+            if (n < 0n)
+                return "\u8CA0" + YllionChineseNotation.numberToWords(-n);
+            else if (n == 0n)
+                return "\u96F6";
+            else if (n < 100n) {
+                return (n >= 10n ? (n >= 20n ? YllionChineseNotation.ONES[Number(n / 10n)] : "") + "\u5341" : "") + YllionChineseNotation.ONES[Number(n % 10n)];
+            }
+            else {
+                const temp = n.toString();
+                const yllionsLen = YllionChineseNotation.YLLIONS.length;
+                if (temp.length > (1 << yllionsLen))
+                    throw new RangeError("Number too large");
+                for (let i = yllionsLen - 1; i >= 1; i--) {
+                    if (temp.length > (1 << i)) {
+                        const split = temp.length - (1 << i);
+                        const high = BigInt(temp.substring(0, split));
+                        const low = BigInt(temp.substring(split));
+                        return (high > 0n ? YllionChineseNotation.numberToWords(high) + YllionChineseNotation.YLLIONS[i] : "") +
+                            (low > 0n ? YllionChineseNotation.numberToWords(low) : "");
+                    }
+                }
+                throw new Error("Unreachable value");
+            }
+        }
+    }
+    YllionChineseNotation.ONES = ["", "\u4E00", "\u4E8C", "\u4E09", "\u56DB", "\u4E94", "\u516D", "\u4E03", "\u516B", "\u4E5D"];
+    YllionChineseNotation.YLLIONS = ["", "\u767E", "\u842C", "\u5104", "\u5146", "\u4EAC", "\u5793", "\u79ED", "\u7A70", "\u6E9D", "\u6F97", "\u6B63", "\u8F09"];
+})(app || (app = {}));
